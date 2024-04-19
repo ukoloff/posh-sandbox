6e44ab76f581ae19b46bc02a0264ddd8e4a2f508
Stas Ukolov <ukoloff@gmail.com>
2024-04-19 09:18:55 +0500

    Active users

--- AD/users180-ldap.ps1
+++ AD/users180-ldap.ps1
@@ -2,5 +2,6 @@
 $date = (Get-Date).AddDays(-180)
 $ticks = $date.ToFileTime()
 
-$ldap = "(&(objectClass=user)(lastLogonTimestamp<=$ticks))"
-Get-ADUser -LDAPFilter $ldap -Properties * | Select-Object -Property name, enabled, lastLogonTimestamp, dn | Out-GridView
+$ldap = "(&(objectClass=user)(lastLogonTimestamp<=$ticks)(!(UserAccountControl:1.2.840.113556.1.4.803:=2)))"
+Get-ADUser -LDAPFilter $ldap | Out-GridView
+# -Properties * | Select-Object -Property name, enabled, lastLogonTimestamp, dn | Out-GridView
