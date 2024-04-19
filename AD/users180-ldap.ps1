25e8707f6145c84b11644e3535a12d1959ff5772
Stas Ukolov <ukoloff@gmail.com>
2024-04-19 10:58:14 +0500

    Add date

--- AD/users180-ldap.ps1
+++ AD/users180-ldap.ps1
@@ -5,6 +5,6 @@ $ticks = $date.ToFileTime()
 $ldap = "(&(objectClass=user)(lastLogonTimestamp<=$ticks)(!(UserAccountControl:1.2.840.113556.1.4.803:=2)))"
 $base = 'OU=EKBH,OU=uxm,OU=MS,DC=omzglobal,DC=com'
 Get-ADUser -LDAPFilter $ldap -SearchBase $base -Properties * `
-  | Select-Object -Property sAMAccountName, name, distinguishedName, lastLogonTimestamp, lastLogon `
+  | Select-Object -Property sAMAccountName, name, distinguishedName, lastLogonDate `
   | Out-GridView
   # | Export-Excel 'aaa.xlsx'
