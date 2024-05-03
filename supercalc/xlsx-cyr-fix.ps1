efb89f05e9a5d30db32b52455047aafa14bef9cf
Stas Ukolov <ukoloff@gmail.com>
2024-05-03 14:43:20 +0500

    Put strings back

--- supercalc/xlsx-cyr-fix.ps1
+++ supercalc/xlsx-cyr-fix.ps1
@@ -29,12 +29,17 @@ $s = $wb.ActiveSheet
 $i850 = [System.Text.Encoding]::GetEncoding('ibm850')
 $cp866 = [System.Text.Encoding]::GetEncoding('cp866')
 
-
 # https://stackoverflow.com/a/78318241/6127481
 $r = $s.range($s.Cells(1, 1), $s.Cells.SpecialCells(11))  # 11 = xlCellTypeLastCell
 
+$i = 0
 foreach ($c in $r) {
   if ($xls.WorksheetFunction.IsText($c)) {
-    Write-Output $cp866.GetString($i850.GetBytes($c.Text))
+    $i += 1
+    Write-Host -NoNewline "`rПерекодируем ячейку $i"
+    $c.value = $cp866.GetString($i850.GetBytes($c.Text))
   }
 }
+
+Write-Output "`nНажмите любую клавищу для завершения..."
+[Console]::ReadKey(1) | Out-Null
