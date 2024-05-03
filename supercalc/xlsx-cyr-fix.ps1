c8755f2d72ae78bd9587bd9d8273771f7fc19b53
Stas Ukolov <ukoloff@gmail.com>
2024-05-03 14:27:52 +0500

    Select all cells

--- supercalc/xlsx-cyr-fix.ps1
+++ supercalc/xlsx-cyr-fix.ps1
@@ -14,7 +14,18 @@ $d = New-Object OpenFileDialog
 $d.Title = "Выберите XLSX-файл для перекодировки русских букв"
 $d.Filter = 'XSLX-файлы|*.xlsx|Все файлы|*.*'
 
-if ($d.ShowDialog() -eq "OK") {
-  Write-Output $d.FileName
-}
+# if ($d.ShowDialog() -eq "OK") {
+#   $src = $d.FileName
+# }
 
+$src = 'C:\Users\s.ukolov\Documents\repo\posh-sandbox\supercalc\test\test.xlsx'
+
+$xls = New-Object -ComObject Excel.Application
+$xls.visible = $true
+
+$wb = $xls.Workbooks.Open($src)
+$s = $wb.ActiveSheet
+
+# https://stackoverflow.com/a/78318241/6127481
+$r = $s.range($s.Cells(1, 1), $s.Cells.SpecialCells(11))  # 11 = xlCellTypeLastCell
+$r.Select()
