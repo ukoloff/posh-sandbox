375f56fc99c275106e70ef309234a9d07bd85958
Stas Ukolov <ukoloff@gmail.com>
2024-05-03 14:20:21 +0500

    Load .xslx

---
+++ supercalc/xlsx-cyr-fix.ps1
@@ -0,0 +1,20 @@
+﻿#
+#  Поправить кодировку русских букв в файле .XLSX,
+#  полученном из SuperCalc:
+#   1. SuperCalc 4
+#   2. Export 1-2-3, расщирение .wk1
+#   3. Открыть в Gnumeric и сохранить в .xlsx
+#   4. Натравить эту утилиту
+#   5. Осмотреть и сохранить
+using namespace System.Windows.Forms
+
+Add-Type -AssemblyName System.Windows.Forms
+
+$d = New-Object OpenFileDialog
+$d.Title = "Выберите XLSX-файл для перекодировки русских букв"
+$d.Filter = 'XSLX-файлы|*.xlsx|Все файлы|*.*'
+
+if ($d.ShowDialog() -eq "OK") {
+  Write-Output $d.FileName
+}
+
