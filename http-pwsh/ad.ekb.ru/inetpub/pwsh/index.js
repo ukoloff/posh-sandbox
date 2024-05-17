645421365f4493e1f3b66e753aef8f7732f5c81a
Stas Ukolov <ukoloff@gmail.com>
2024-05-17 08:55:38 +0500

    Get request body

--- http-pwsh/ad.ekb.ru/inetpub/pwsh/index.js
+++ http-pwsh/ad.ekb.ru/inetpub/pwsh/index.js
@@ -8,8 +8,14 @@ www.listen(80)
 function handler(req, resp) {
   console.log(req.method, req.url)
   resp.setHeader('Content-Type', 'application/json')
-  resp.end(JSON.stringify({
-    msg: "Hello, world!",
-    ctime: new Date,
-  }))
+  var body = ''
+  req.on('data', chunk => body += chunk)
+  req.on('end', chunk => {
+    resp.end(JSON.stringify({
+      method: req.method,
+      msg: "Hello, world!",
+      ctime: new Date,
+      data: JSON.parse(body)
+    }))
+  })
 }
