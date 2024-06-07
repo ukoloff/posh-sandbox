b6b967e9a08348059dfc2c572d2a27cc90813296
Stas Ukolov <ukoloff@gmail.com>
2024-06-07 10:18:40 +0500

    Save photos

--- AD/photo/z15.ps1
+++ AD/photo/z15.ps1
@@ -1,4 +1,14 @@
-﻿$ldap = "((!(userAccountControl:1.2.840.113556.1.4.803:=2))(jpegPhoto=*))"
+﻿#
+# Save photos to files
+#
+$src = Split-Path ($MyInvocation.MyCommand.Source) -Parent
+$dst = Join-Path $src 'jpeg'
+
+$ldap = "((!(userAccountControl:1.2.840.113556.1.4.803:=2))(jpegPhoto=*))"
 $base = 'OU=Цех №15,OU=Дирекция по производству,OU=Дирекция,OU=Users,OU=EKBH,OU=uxm,OU=MS,DC=omzglobal,DC=com'
 Get-ADUser -LDAPFilter $ldap -SearchBase $base -Properties * `
-    | Out-GridView
+| % {
+  # https://github.com/chrisdee/Scripts/blob/master/PowerShell/Working/AD/GetADUserThumbnailPhotos.ps1
+  $fname = Join-Path $dst ($_.SamAccountName + '.jpg')
+  [System.Io.File]::WriteAllBytes($fname, $_.jpegPhoto[0])
+}
