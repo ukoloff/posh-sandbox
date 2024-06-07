43d50f47aaff407af0f03c1686e9b44d11490d28
Stas Ukolov <ukoloff@gmail.com>
2024-06-07 09:54:12 +0500

    List users with photo

---
+++ AD/photo/z15.ps1
@@ -0,0 +1,4 @@
+﻿$ldap = "((!(userAccountControl:1.2.840.113556.1.4.803:=2))(jpegPhoto=*))"
+$base = 'OU=Цех №15,OU=Дирекция по производству,OU=Дирекция,OU=Users,OU=EKBH,OU=uxm,OU=MS,DC=omzglobal,DC=com'
+Get-ADUser -LDAPFilter $ldap -SearchBase $base -Properties * `
+    | Out-GridView
