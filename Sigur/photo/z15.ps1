7d3986b33870ad1c9dbed555343dce914af15795
Stas Ukolov <ukoloff@gmail.com>
2024-06-11 11:16:30 +0500

    Fetch photos

--- Sigur/photo/z15.ps1
+++ Sigur/photo/z15.ps1
@@ -5,5 +5,18 @@
 
 $cred = Get-StoredCredential -Target 'mysql:SKUD'
 
-$sql = Open-MySqlConnection -ConnectionName Sigur -Server srvskud-ekbh1 -Port 3305 -Database  tc-db-main -Credential $cred
+$sql = Open-MySqlConnection -ConnectionName default -Server srvskud-ekbh1 -Port 3305 -Database  tc-db-main -Credential $cred
 
+$data = Invoke-SqlQuery @"
+Select
+  P.ID,
+  HIRES_RASTER,
+  NAME, TABID
+From
+  photo As X
+  Join personal P On P.ID = X.ID
+Where
+  P.PARENT_ID = 6231
+"@
+
+echo $data
