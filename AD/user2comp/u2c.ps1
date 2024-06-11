515ce063395963ba5b24293897b447ad67155b69
Stas Ukolov <ukoloff@gmail.com>
2024-06-11 10:25:20 +0500

    Add Dept

--- AD/user2comp/u2c.ps1
+++ AD/user2comp/u2c.ps1
@@ -13,11 +13,12 @@ Get-ADComputer -SearchBase $cDN -SearchScope OneLevel -LDAPFilter '(managedBy=*)
 }
 
 foreach ($dn in $uDNs) {
-  Get-ADUser -SearchBase $dn -LDAPFilter '(!userAccountControl:1.2.840.113556.1.4.803:=2)' -Properties displayName| % {
+  Get-ADUser -SearchBase $dn -LDAPFilter '(!userAccountControl:1.2.840.113556.1.4.803:=2)' -Properties displayName,canonicalName | % {
     [PSCustomObject]@{
       id = $_.sAMAccountName;
       fio = $_.Name;
       name = $_.displayName;
+      dept = ($_.CanonicalName -split '/')[-2];
       host = $map[$_.DistinguishedName];
     }
   } | Sort-Object id | Export-Excel
