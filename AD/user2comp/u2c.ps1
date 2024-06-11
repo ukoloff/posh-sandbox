1bc650fbfbe6e9e165bb27b31d56532051546541
Stas Ukolov <ukoloff@gmail.com>
2024-06-11 10:00:25 +0500

    Build User 2 Computer map

---
+++ AD/user2comp/u2c.ps1
@@ -0,0 +1,15 @@
+﻿
+$uDNs = @(
+  'OU=ОГК,OU=Техническая дирекция,OU=Дирекция,OU=Users,OU=EKBH,OU=uxm,OU=MS,DC=omzglobal,DC=com',
+  'OU=ОГТ,OU=Техническая дирекция,OU=Дирекция,OU=Users,OU=EKBH,OU=uxm,OU=MS,DC=omzglobal,DC=com'
+)
+
+$cDN = 'OU=Computers,OU=EKBH,OU=uxm,OU=MS,DC=omzglobal,DC=com'
+
+$map = @{}
+
+Get-ADComputer -SearchBase $cDN -SearchScope OneLevel -LDAPFilter '(managedBy=*)' -Properties managedBy | % {
+  $map[$_.ManagedBy] = $_.Name
+}
+
+echo $map
