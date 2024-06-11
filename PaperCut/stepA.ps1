6b13ff764f7c1c304cd35fd86699e1af344ed0b0
Stas Ukolov <ukoloff@gmail.com>
2024-06-11 15:25:51 +0500

    Create destination folder

--- PaperCut/stepA.ps1
+++ PaperCut/stepA.ps1
@@ -1,6 +1,12 @@
-$src = "\\omzglobal.com\uxm\Exchange\PrintStat\Logs\Daily"
+$root = "\\omzglobal.com\uxm\Exchange\PrintStat\Logs"
+$src = Join-Path $root Daily
+$dst = Join-Path $root Aggregate
 
-function readDay($date = [datetime]::Now) {
+if (!(Test-Path $dst -PathType Container)) {
+  mkdir $dst -Force | Out-Null
+}
+
+function readDay([datetime]$date = [datetime]::Now) {
   $fname = $src + "\PrintLog-" + $date.ToString("dd-MM-yyyy") + ".csv"
   $grep = "^" + $date.ToString("yyyy-MM-dd") + "\s"
   if (!(Test-Path $fname -PathType Leaf)) {
