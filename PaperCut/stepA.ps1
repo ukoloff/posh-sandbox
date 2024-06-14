db396c97fd4443a38e301d3a2b26131ebfe69eb5
Stas Ukolov <ukoloff@gmail.com>
2024-06-14 15:44:26 +0500

    Read all days

--- PaperCut/stepA.ps1
+++ PaperCut/stepA.ps1
@@ -10,13 +10,6 @@ if (!(Test-Path $dst -PathType Container)) {
   mkdir $dst -Force | Out-Null
 }
 
-if ($all) {
-  $data = readAllDays
-}
-else {
-  $data = readDays($days)
-}
-
 function readDay([datetime]$date = [datetime]::Now) {
   $fname = $src + "\PrintLog-" + $date.ToString("dd-MM-yyyy") + ".csv"
   $grep = "^" + $date.ToString("yyyy-MM-dd") + "\s"
@@ -33,4 +26,24 @@ function readDays($days) {
     $lines += readDay($today.AddDays(1 - $i))
   }
   $lines
+}
+
+function readAllDays() {
+  $lines = @()
+  $csvs = Get-ChildItem $src -Filter 'PrintLog-*.csv'
+  foreach ($f in $csvs) {
+    if ($f -match "-(?<d>\d{2})-(?<m>\d{2})-(?<y>\d{4}).") {
+      [datetime]$d = Get-Date -Day $Matches.d -Month $Matches.m -Year $Matches.y
+      $lines += readDay($d)
+    }
+  }
+  $lines
+}
+
+# $all = $true
+if ($all) {
+  $data = readAllDays
+}
+else {
+  $data = readDays($days)
 }
