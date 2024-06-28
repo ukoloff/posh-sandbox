595b7acbdda9889ec5d6aa828ee1d2fdd12382ed
Stas Ukolov <ukoloff@gmail.com>
2024-06-28 13:14:43 +0500

    Allow stored credentials

--- PaperCut/pg.ps1
+++ PaperCut/pg.ps1
@@ -3,6 +3,13 @@
 #
 
 # Install-Module -Name SimplySql
+
+# Store Credentials:
+# ------------------
+# $cred = Get-Credential
+# New-StoredCredential -Target pqsql:UXM -Credentials $cred -Persist LocalMachine
+$cred = Get-StoredCredential -Target pqsql:UXM  # Try Kerberos if not found
+
 Open-PostGreConnection -Server 'pg.ekb.ru' -Database uxm -Credential $null
 
 $data = Invoke-SqlQuery "Select * from papercut" -AsDataTable
