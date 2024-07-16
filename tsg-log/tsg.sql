ecdbaf4addce5b7504ccf36a1edf3b40f50a2b92
Stas Ukolov <ukoloff@gmail.com>
2024-07-16 13:34:01 +0500

    Add TSG table

--- tsg-log/tsg.sql
+++ tsg-log/tsg.sql
@@ -8,3 +8,27 @@ CREATE INDEX tsg_log_ctime_idx ON tsg_log USING btree (ctime);
 -- Permissions
 GRANT SELECT ON TABLE tsg_log TO "uxmR";
 GRANT UPDATE, DELETE, INSERT ON TABLE tsg_log TO "uxmW";
+
+CREATE TABLE tsg (
+	id int generated always as identity NOT NULL,
+	rec_id int NULL,
+	"start" timestamptz NULL,
+	"end" timestamptz NULL,
+	duration int4 NULL,
+	log_id int4 NULL,
+	guid text NULL,
+	ip text NULL,
+	"user" text NULL,
+	host text NULL,
+	proto text NULL,
+	inb int NULL,
+	outb int NULL,
+	CONSTRAINT tsg_pkey PRIMARY KEY (id)
+);
+CREATE INDEX tsg_start_idx ON public.tsg USING btree (start);
+CREATE INDEX tsg_user_idx ON public.tsg USING btree ("user", start);
+ALTER TABLE public.tsg ADD CONSTRAINT tsg_tsg_log_fk FOREIGN KEY (log_id) REFERENCES tsg_log(id);
+
+-- Permissions
+GRANT SELECT ON TABLE public.tsg TO "uxmR";
+GRANT UPDATE, DELETE, INSERT ON TABLE public.tsg TO "uxmW";
