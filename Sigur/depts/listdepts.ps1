9f569b9b7a5686fa0fcc1a1d20bd394c7e10999f
Stas Ukolov <ukoloff@gmail.com>
2024-08-13 11:58:45 +0500

    Read XLSX

---
+++ Sigur/depts/listdepts.ps1
@@ -0,0 +1,8 @@
+﻿#
+# https://devblogs.microsoft.com/scripting/grabbing-excel-xlsx-values-with-powershell/
+#
+# Install-module PSExcel
+#
+$dir = Split-Path $PSCommandPath -Parent
+$xlsx = Join-Path $dir 'структура.xlsx'
+$data = Import-XLSX $xlsx
