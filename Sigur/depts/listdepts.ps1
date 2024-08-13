c2d897edb96a45f66a72402881cf8b6db1557cd2
Stas Ukolov <ukoloff@gmail.com>
2024-08-13 12:04:45 +0500

    Connect to MySQL

--- Sigur/depts/listdepts.ps1
+++ Sigur/depts/listdepts.ps1
@@ -6,3 +6,14 @@
 $dir = Split-Path $PSCommandPath -Parent
 $xlsx = Join-Path $dir 'структура.xlsx'
 $data = Import-XLSX $xlsx
+
+$cred = Get-StoredCredential -Target 'mysql:SKUD'
+
+$sql = Open-MySqlConnection -Server srvskud-ekbh1 -Port 3305 -Database tc-db-main -Credential $cred
+
+foreach ($row in $data) {
+  $id = $row.Номер -replace '\D+', ''
+  echo $id
+}
+
+Close-SqlConnection
