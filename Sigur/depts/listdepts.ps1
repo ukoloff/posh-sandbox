b43b1a899faffbf9f2a793bf0a045d91a942f6b2
Stas Ukolov <ukoloff@gmail.com>
2024-08-13 13:39:40 +0500

    List Depts

--- Sigur/depts/listdepts.ps1
+++ Sigur/depts/listdepts.ps1
@@ -9,11 +9,31 @@ $data = Import-XLSX $xlsx
 
 $cred = Get-StoredCredential -Target 'mysql:SKUD'
 
-$sql = Open-MySqlConnection -Server srvskud-ekbh1 -Port 3305 -Database tc-db-main -Credential $cred
-
+Open-MySqlConnection -Server srvskud-ekbh1 -Port 3305 -Database tc-db-main -Credential $cred
+$depts = Invoke-SqlQuery @"
+  select
+    ID,
+    NAME,
+    regexp_substr(NAME, '^[0-9]+') as kod
+  from
+    personal as Dpt
+  where
+    ` TYPE ` = 'DEP'
+    and name rlike '^[0-9]+[[:space:]]'
+    and STATUS = 'AVAILABLE'
+"@
+$deptIndex = @{}
+foreach ($d in $depts) {
+  $deptIndex[$d.kod] = $d.ID
+}
+$depts = @{}
 foreach ($row in $data) {
   $id = $row.Номер -replace '\D+', ''
-  echo $id
+  if (!$deptIndex[$id]) { continue }
+  $depts[$deptIndex[$id]] = 1
 }
+$deptIndex = 0
+$depts = $depts.Keys | Sort-Object
+Write-Output "($($depts -join ', '))"
 
 Close-SqlConnection
