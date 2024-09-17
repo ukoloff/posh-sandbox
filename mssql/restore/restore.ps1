27b54ef914a399163ff817e072195578c4e792b0
Stas Ukolov <ukoloff@gmail.com>
2024-09-17 13:33:50 +0500

    Rename files to restore

--- mssql/restore/restore.ps1
+++ mssql/restore/restore.ps1
@@ -7,8 +7,8 @@ $Server = "SRVSQL-1C"
 $DB = 'stas2'
 $Backup = 'e:\stas\Stas_20240916_164915.bak'
 
-$reData = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("stas", "j:\stas\stas2.mdf")
-$reLog = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("stas_log", "j:\stas\stas2_log.ldf")
+$reData = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("stas", "j:\stas\stas.mdf")
+$reLog = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("stas_log", "j:\stas\stas_log.ldf")
 $re = @($reData,$reLog)
 
 Restore-SqlDatabase -ServerInstance $Server -Database $DB -BackupFile $Backup -ReplaceDatabase -RelocateFile $re
