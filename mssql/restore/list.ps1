94c5db890a2acfedd40c234dc885794f7cb880ee
Stas Ukolov <ukoloff@gmail.com>
2024-09-19 13:40:30 +0500

    Find latest backup(s)

--- mssql/restore/list.ps1
+++ mssql/restore/list.ps1
@@ -8,10 +8,22 @@ Open-SQLConnection -Server $Server
 
 # $backups = @()
 
-Get-ChildItem $Folder -File |
+$baks = Get-ChildItem $Folder -File |
 Sort-Object CreationTime -Descending |
 ForEach-Object {
   Invoke-SqlQuery 'restore headeronly from disk = @file' -Parameters @{file = $_.FullName }
-} | Out-GridView
+}
+$found = @()
+$full = $baks | Where-Object { $_.BackupType -eq 1 }
+if ($full) {
+  $full = $full[0]
+  $found = @($full)
+  $inc = $baks | Where-Object { $_.BackupType -eq 5 -and $_.DifferentialBaseGUID -eq $full.BackupSetGUID}
+  if ($inc) {
+    $found += ($inc[0])
+  }
+}
 
 Close-SqlConnection
+
+echo $found
