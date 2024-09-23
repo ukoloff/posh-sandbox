dc403a7e4670236f3363ad257bb7bdc592f3a727
Stas Ukolov <ukoloff@gmail.com>
2024-09-23 13:21:15 +0500

    Send email from PowerShell

---
+++ 1c2AD/birthday/bday.ps1
@@ -0,0 +1,26 @@
+﻿#
+# Рассылка уведомлений о днях рождения
+#
+
+
+# Store Credentials:
+# ------------------
+# $cred = Get-Credential
+# # Install-Module -Name CredentialManager
+# New-StoredCredential -Target serviceuxm@omzglobal.com -Credentials $cred -Persist LocalMachine
+$cred = Get-StoredCredential -Target serviceuxm@omzglobal.com
+
+$mail = @{
+  Body = 'Проверка связи'
+  # Attachments = $Attachment0.FullName,$Attachment1.FullName
+  # BodyAsHtml = $true
+  Subject = "Поздравляем с днем рождения. Пух"
+  From = 'serviceuxm@omzglobal.com'
+  To = 'Stanislav.Ukolov@omzglobal.com'
+  SmtpServer = 'srvmail-ekbh5.omzglobal.com'
+  Port = '2525'
+  Encoding  = 'UTF8'
+  Credential = $cred
+}
+
+Send-MailMessage @mail
