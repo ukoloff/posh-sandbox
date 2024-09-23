06c39197d3183628e5d1bd90c3e5d32f5ba6f9e8
Stas Ukolov <ukoloff@gmail.com>
2024-09-23 14:31:03 +0500

    List users to warn

---
+++ 1c2AD/birthday/bday_before.ps1
@@ -0,0 +1,10 @@
+﻿#
+# Уведомления перед днём рождения
+#
+Import-Module ActiveDirectory
+$adBase = 'OU=EKBH,OU=uxm,OU=MS,DC=omzglobal,DC=com'
+
+$then = (Get-Date).AddDays(4).ToString("dd.MM")
+$filter = "(&(!userAccountControl:1.2.840.113556.1.4.803:=2)(extensionAttribute1=$then.*)(mail=*))"
+[array]$Users = Get-ADUser -SearchBase $adBase -LDAPFilter $filter -Properties mail, middleName
+$Users
