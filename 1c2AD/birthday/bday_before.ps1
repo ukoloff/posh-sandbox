d4b3455195e2dabf6da76b774d84dc16135af01b
Stas Ukolov <ukoloff@gmail.com>
2024-09-24 15:45:20 +0500

    GetManagers is a method

--- 1c2AD/birthday/bday_before.ps1
+++ 1c2AD/birthday/bday_before.ps1
@@ -7,7 +7,7 @@ $adBase = 'OU=uxm,OU=MS,DC=omzglobal,DC=com'
 $then = (Get-Date).AddDays(4).ToString("dd.MM")
 $filter = "(&(!userAccountControl:1.2.840.113556.1.4.803:=2)(extensionAttribute1=$then.*)(mail=*))"
 [array]$Users = Get-ADUser -SearchBase $adBase -LDAPFilter $filter -Properties manager
-$Users
+# $Users
 
 class Send2 {
   [Microsoft.ActiveDirectory.Management.ADUser] $u
@@ -15,45 +15,47 @@ class Send2 {
 
   Send2($user) {
     $this.u = Get-ADUser $user -Properties Manager
+    $this.getManagers()
   }
-}
 
-$u = 'P.Vazhenin'
-$u = 's.ukolov'
-$u = 'lobza'
-$u = 'gretskaya'
-
-$z = [Send2]::new($u)
-
-function getManager($adUser) {
-  $adUser = Get-ADUser $adUser -Properties Manager
-  $manager = $adUser.Manager
-  if ($manager) {
-    return @($manager)
-  }
-  $mgrFilter = "(&(!userAccountControl:1.2.840.113556.1.4.803:=2)(mail=*)(directReports=*))"
-  $dn = $adUser.DistinguishedName
-  while (1) {
-    $ou = 'OU=' + ($dn -split ',OU=', 2)[1]
-    if (!$ou.EndsWith($adBase)) { break }
-    [array]$list = Get-ADUser -SearchBase $ou -LDAPFilter $mgrFilter -SearchScope OneLevel
-    if ($list) {
-      [array]$managers = $list | ForEach-Object { $_.DistinguishedName }
-      return $managers
+  [void] getManagers() {
+    $manager = $this.u.Manager
+    if ($manager) {
+      $this.managers = @($manager)
+      return
     }
-    $dn = $ou
+    $mgrFilter = "(&(!userAccountControl:1.2.840.113556.1.4.803:=2)(mail=*)(directReports=*))"
+    $dn = $this.u.DistinguishedName
+    while (1) {
+      $ou = 'OU=' + ($dn -split ',OU=', 2)[1]
+      if (!$ou.EndsWith($global:adBase)) { break }
+      [array]$list = Get-ADUser -SearchBase $ou -LDAPFilter $mgrFilter -SearchScope OneLevel
+      if ($list) {
+        [array]$mgrs = $list | ForEach-Object { $_.DistinguishedName }
+        $this.managers = $mgrs
+        return
+      }
+      $dn = $ou
+    }
+    $this.managers = @()
   }
-  return @()
 }
 
 function listManagers() {
   [array]$users = Get-ADUser -SearchBase $adBase -LDAPFilter "(&(!userAccountControl:1.2.840.113556.1.4.803:=2)(extensionAttribute1=*)(mail=*))"
   foreach ($u in $users) {
-    $ms = getManager($u)
+    $ms = [Send2]::new($u).managers
     if ($ms.count -eq 1) { continue }
     Write-Output "$($u.SamAccountName): $($ms.ForEach({ (Get-ADUser $_).SamAccountName }) -join ', ')"
   }
 }
 
 # listManagers
-getManager($u) | Out-GridView
+
+$u = 'P.Vazhenin'
+$u = 's.ukolov'
+$u = 'lobza'
+# $u = 'gretskaya'
+
+$z = [Send2]::new($u)
+$z.managers | Out-GridView
