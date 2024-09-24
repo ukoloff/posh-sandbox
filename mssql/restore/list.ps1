767d8cb1f11f08c5f6bb8c671e0420e151441c63
Stas Ukolov <ukoloff@gmail.com>
2024-09-24 10:01:04 +0500

    Faster backups search

--- mssql/restore/list.ps1
+++ mssql/restore/list.ps1
@@ -6,50 +6,34 @@ $Folder = '\\srvsql-1c\e$\'
 $DBs = @('stas', 'OP_WORK', 'ERP_WORK', 'ZUP_20')
 
 function getBackups($folder) {
-  $baks = Get-ChildItem $folder -File |
-  Sort-Object CreationTime -Descending |
-  ForEach-Object {
-    $row = Invoke-SqlQuery 'restore headeronly from disk = @file' -Parameters @{file = $_.FullName }
-    $row | Add-Member -NotePropertyName FullName -NotePropertyValue $_.FullName
-    $row
-  }
-  $found = @()
-  [array]$full = $baks | Where-Object {
-    $_.BackupType -eq 1 }
-  if ($full) {
-    $full = $full[0]
-    $found = @($full)
-    [array]$inc = $baks | Where-Object {
-      $_.BackupType -eq 5 -and
-      $_.DifferentialBaseGUID -eq $full.BackupSetGUID }
-    if ($inc) {
-      $found += ($inc[0])
+  $diffs = @{}
+  [array]$baks = Get-ChildItem $folder -File |
+  Sort-Object CreationTime -Descending
+  foreach($bak in $baks) {
+    $row = Invoke-SqlQuery 'restore headeronly from disk = @file' -Parameters @{file = $bak.FullName }
+    $row | Add-Member -NotePropertyName FullName -NotePropertyValue $bak.FullName
+    switch ($row.BackupType) {
+      1 {
+        # Full DB backup
+        $result = @($row)
+        if ($diffs[$row.BackupSetGUID]) {
+          $result += @($diffs[$row.BackupSetGUID])
+        }
+        return $result
+      }
+      5 {
+        # Incremental DB backup
+        if (!$diffs[$row.DifferentialBaseGUID]) {
+          $diffs[$row.DifferentialBaseGUID] = $row
+        }
+        continue
+      }
     }
-  }
-  $found
-}
-
-
-$baks = Get-ChildItem $Folder -File |
-Sort-Object CreationTime -Descending |
-ForEach-Object {
-  $row = Invoke-SqlQuery 'restore headeronly from disk = @file' -Parameters @{file = $_.FullName }
-  $row | Add-Member -NotePropertyName FullName -NotePropertyValue $_.FullName
-  $row
-}
-$found = @()
-[array]$full = $baks | Where-Object { $_.BackupType -eq 1 }
-if ($full) {
-  $full = $full[0]
-  $found = @($full)
-  [array]$inc = $baks | Where-Object { $_.BackupType -eq 5 -and $_.DifferentialBaseGUID -eq $full.BackupSetGUID }
-  if ($inc) {
-    $found += ($inc[0])
   }
 }
 
 Open-SQLConnection -Server $Server
 foreach ($db in $DBs) {
-  getBackups("$Folder/$db") | Format-Table -Property FullName, BackupType, BackupStartDate
+  getBackups("$Folder$db") | Format-Table -Property FullName, BackupType, BackupStartDate
 }
 Close-SqlConnection
