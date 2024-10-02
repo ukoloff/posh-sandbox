f5020d904a7778ce0b5221278f757a12c6147a82
Stas Ukolov <ukoloff@gmail.com>
2024-10-02 16:36:45 +0500

    Import Ideco certificate

--- VPN/uxm-vpn.ps1
+++ VPN/uxm-vpn.ps1
@@ -6,3 +6,21 @@ function isAdmin {
   $user.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
+$crt = @"
+-----BEGIN CERTIFICATE-----
+MIIBvDCCAWKgAwIBAgIUJYGeuYqtuYrRYCji+q0kgdwYmdwwCgYIKoZIzj0EAwIw
+LTEXMBUGA1UEBRMOMjAyMzAzMjQxNDEwMTQxEjAQBgNVBAMMCUlkZWNvIFVUTTAe
+Fw0yMzAzMjMxNDEwMTRaFw0zMzAzMjExNDEwMTRaMC0xFzAVBgNVBAUTDjIwMjMw
+MzI0MTQxMDE0MRIwEAYDVQQDDAlJZGVjbyBVVE0wWTATBgcqhkjOPQIBBggqhkjO
+PQMBBwNCAAQM8105PHGsEsdFG2RIiB84QfP5XuE688sznhKAmSrGsn99IAJBIy+N
+Xf9k/iex7RtAtTX1L2ZjdN5A7nm7IuCno2AwXjAdBgNVHQ4EFgQUFHd0hAps4nit
+YSJObt3lTJLsQYcwDwYDVR0TAQH/BAUwAwEB/zALBgNVHQ8EBAMCAYYwHwYDVR0j
+BBgwFoAUFHd0hAps4nitYSJObt3lTJLsQYcwCgYIKoZIzj0EAwIDSAAwRQIgbWDB
+808Jx5amkrL6iDVgpsn6wQlW3UmdBlGC/w3fTIoCIQCvw883p118hGDBrM/yiMvI
+QWQ2VGVVIXdglwQT7ynYKg==
+-----END CERTIFICATE-----
+"@.Trim()
+
+$t = New-TemporaryFile
+Set-Content $t $crt
+Import-Certificate -FilePath $t.FullName -CertStoreLocation Cert:\LocalMachine\Root
