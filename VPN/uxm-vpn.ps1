bac803ead5019a2ba0c44d6532b01af929b40c74
Stas Ukolov <ukoloff@gmail.com>
2024-10-02 16:25:29 +0500

    Check for elevated

--- VPN/uxm-vpn.ps1
+++ VPN/uxm-vpn.ps1
@@ -1,3 +1,8 @@
 ﻿#
 # Настройка VPN @Уралхиммаш
 #
+function isAdmin {
+  $user = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  $user.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
