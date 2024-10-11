4326203d70145257d97e73a4e885f1bace53d64d
Stas Ukolov <ukoloff@gmail.com>
2024-10-11 16:09:55 +0500

    Clear tables

--- mssql/replicate.bak/bak-replica.ps1
+++ mssql/replicate.bak/bak-replica.ps1
@@ -5,11 +5,17 @@
 $src = "SRVSQL-1C"
 $dst = "SRVSQL-1Ctests"
 
+$tables = -split "backupfile backupfilegroup backupset backupmediafamily backupmediaset"
+
 Open-SQLConnection -ConnectionName xa -Server $src -Database msdb
 Open-SQLConnection -ConnectionName xz -Server $dst -Database msdb
 
 $table = Invoke-SqlQuery -ConnectionName xa "Select * From backupmediaset"
-$table
+$table[0]
+
+foreach ($t in $tables) {
+  Invoke-SqlUpdate -ConnectionName xz "Delete From $t"
+}
 
 Close-SqlConnection xa
 Close-SqlConnection xz
