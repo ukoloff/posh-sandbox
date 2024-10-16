1d90fba33ddc4f8ffb999bce9fd378da224125ec
Stas Ukolov <ukoloff@gmail.com>
2024-10-16 10:23:44 +0500

    Add self-installation as Scheduled Task

--- mssql/replicate.bak/bak-replica.ps1
+++ mssql/replicate.bak/bak-replica.ps1
@@ -1,12 +1,32 @@
 #
 # Перенос журналов резервного копирования с одного SQL-сервера на другой
 #
+param(
+  [switch]$install,
+  [switch]$remove
+)
 
 $src = "SRVSQL-1C"
 $dst = "SRVSQL-1Ctests"
 
 $tables = -split "backupfile backupfilegroup backupset backupmediafamily backupmediaset"
 $rtables = -split "restorehistory restorefilegroup restorefile"
+
+if ($install) {
+  $me = Split-Path $PSCommandPath -Leaf
+  $dir = Split-Path $PSCommandPath -Parent
+  $Action = New-ScheduledTaskAction -Execute "powershell" -Argument ".\$me" -WorkingDirectory $dir
+  $Trigger = New-ScheduledTaskTrigger -Daily -At 7:40 -RandomDelay 00:05:00
+  $Task = New-ScheduledTask -Action $Action -Trigger $Trigger
+  Register-ScheduledTask -TaskName $me -TaskPath uxm -InputObject $Task -User "System" -Force
+  exit
+}
+
+if ($remove) {
+  $me = Split-Path $PSCommandPath -Leaf
+  Unregister-ScheduledTask -TaskName $me -TaskPath '\uxm\' -Confirm:$false
+  exit
+}
 
 Open-SQLConnection -ConnectionName xa -Server $src -Database msdb
 Open-SQLConnection -ConnectionName xz -Server $dst -Database msdb
