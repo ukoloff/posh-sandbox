7bed5898c89c045e30188df38ed701722c658e3f
Stas Ukolov <ukoloff@gmail.com>
2024-10-23 13:19:18 +0500

    Fetch installed Kompas(es)

---
+++ kompas/update-kompas.ps1
@@ -0,0 +1,7 @@
+﻿#
+# Обновление Компас-3D
+#
+$GUID = '{05AB476A-CCCF-456F-B37F-43DDD7AE5F72}'
+$v = '21.0.0'
+
+[array]$kompas = Get-WmiObject -Class "Win32_Product" -Filter "IdentifyingNumber='$GUID'"
