cd1f9b890ef0ec4d5932c998816a9a6f102b8d74
Stas Ukolov <ukoloff@gmail.com>
2024-10-23 13:44:01 +0500

    Check version & update

--- kompas/update-kompas.ps1
+++ kompas/update-kompas.ps1
@@ -3,5 +3,32 @@
 #
 $GUID = '{05AB476A-CCCF-456F-B37F-43DDD7AE5F72}'
 $v = '21.0.0'
+$dist = '\\service\Soft\kompasspatch\KOMPAS-3D_v21.0.26_x64.msp'
 
+$Log = @{
+  LiteralPath =  Join-Path $env:TEMP "$(Split-Path $PSCommandPath -Leaf).log"
+  Append      = $true
+}
+
+function timeStamp() {
+  Get-Date -UFormat '%Y-%m-%d %T'
+}
+
+"Starting: $(timeStamp)" | Out-File @Log
 [array]$kompas = Get-WmiObject -Class "Win32_Product" -Filter "IdentifyingNumber='$GUID'"
+"Kompas versions found:" | Out-File @Log
+$kompas | Format-List | Out-File @Log
+
+$cnt = ($kompas | ForEach-Object { $_.Version.StartsWith("$v.")}).Count
+"Version(s) to upgrade: $cnt" | Out-File @Log
+
+if (!$cnt) {
+  "Exiting..." | Out-File @Log
+  exit
+}
+
+"Installing: $dist" | Out-File @Log
+
+msiexec /update $dist /quiet
+
+"Done: $(timeStamp)" | Out-File @Log
