651abfed605a0015dcfe94de4283b30cb8dbd0e9
Stas Ukolov <ukoloff@gmail.com>
2024-10-23 14:00:59 +0500

    Add central log

--- kompas/update-kompas.ps1
+++ kompas/update-kompas.ps1
@@ -5,8 +5,15 @@ $GUID = '{05AB476A-CCCF-456F-B37F-43DDD7AE5F72}'
 $v = '21.0.0'
 $dist = '\\service\Soft\kompasspatch\KOMPAS-3D_v21.0.26_x64.msp'
 
+# Лог на компьютере пользователя
 $Log = @{
   LiteralPath =  Join-Path $env:TEMP "$(Split-Path $PSCommandPath -Leaf).log"
+  Append      = $true
+}
+
+# Центральный лог, возле дистрибутива
+$CentralLog = @{
+  LiteralPath = Join-Path (Join-Path (Split-Path $dist -Parent) Logs) "$($env:COMPUTERNAME).log"
   Append      = $true
 }
 
@@ -15,12 +22,15 @@ function timeStamp() {
 }
 
 "Starting: $(timeStamp)" | Out-File @Log
+"[$(timeStamp)] Starting..." | Out-File @CentralLog
+
 [array]$kompas = Get-WmiObject -Class "Win32_Product" -Filter "IdentifyingNumber='$GUID'"
 "Kompas version(s) found:" | Out-File @Log
 $kompas | Format-List | Out-File @Log
 
 $cnt = ($kompas | ForEach-Object { $_.Version.StartsWith("$v.")}).Count
 "Version(s) to upgrade: $cnt" | Out-File @Log
+"[$(timeStamp)] Version(s) to upgrade: $cnt" | Out-File @CentralLog
 
 if (!$cnt) {
   "Exiting..." | Out-File @Log
@@ -28,7 +38,9 @@ if (!$cnt) {
 }
 
 "Installing: $dist" | Out-File @Log
+"[$(timeStamp)] Installing: $dist" | Out-File @CentralLog
 
 msiexec /update $dist /quiet
 
 "Done: $(timeStamp)" | Out-File @Log
+"[$(timeStamp)] Done" | Out-File @CentralLog
