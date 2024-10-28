adf87b1f1ebf7ee74e6dcc9a87d61603ce5c96c7
Stas Ukolov <ukoloff@gmail.com>
2024-10-28 13:29:49 +0500

    Make restart work

--- kompas/restart/restart.ps1
+++ kompas/restart/restart.ps1
@@ -5,10 +5,11 @@
 $who = 'Win10-3'
 
 Invoke-Command -ComputerName $who -ScriptBlock {
-  $Action = New-ScheduledTaskAction -Execute "powershell" -Argument "-c 'Restart-Computer -Force'"
+  $Action = New-ScheduledTaskAction -Execute "powershell" -Argument "-c Restart-Computer -Force"
   $Trigger = New-ScheduledTaskTrigger -Once -At 01:23
+  $Trigger.RandomDelay = "PT5H"
   $Settings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter 00:01:00
   $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
-  $Task.Triggers[0].EndBoundary = (Get-Date).AddMinutes(100).ToString('s')
-  Register-ScheduledTask -TaskName "Restart!" -TaskPath uxm -InputObject $Task -User "System" -Force | Out-Null
+  $Task.Triggers[0].EndBoundary = (Get-Date).AddMinutes(1).ToString('s')
+  Register-ScheduledTask -TaskName "updateKompas" -TaskPath uxm -InputObject $Task -User "System" -Force | Out-Null
 }
