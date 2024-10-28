b9cc2ce458b44c9cfd6574018e7ed941e9a8a0f0
Stas Ukolov <ukoloff@gmail.com>
2024-10-28 13:51:37 +0500

    Fix date

--- kompas/restart/restart.ps1
+++ kompas/restart/restart.ps1
@@ -5,11 +5,17 @@
 $who = 'Win10-3'
 
 Invoke-Command -ComputerName $who -ScriptBlock {
+  $now = Get-Date
+  $at = Get-Date "$(Get-Date $now -UFormat %Y-%m-%d) 01:23:00"
+  if ($now -ge $at) {
+    $at = $at.AddDays(1)
+  }
+  $hours = 5
   $Action = New-ScheduledTaskAction -Execute "powershell" -Argument "-c Restart-Computer -Force"
-  $Trigger = New-ScheduledTaskTrigger -Once -At 01:23
-  $Trigger.RandomDelay = "PT5H"
+  $Trigger = New-ScheduledTaskTrigger -Once -At $at
+  $Trigger.RandomDelay = "PT$($hours)H"
   $Settings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter 00:01:00
   $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
-  $Task.Triggers[0].EndBoundary = (Get-Date).AddMinutes(1).ToString('s')
+  $Task.Triggers[0].EndBoundary = $at.AddHours($hours + 1).ToString('s')
   Register-ScheduledTask -TaskName "updateKompas" -TaskPath uxm -InputObject $Task -User "System" -Force | Out-Null
 }
