78111b5df4fc07515341a8d8f7d8562615cb2d27
Stas Ukolov <ukoloff@gmail.com>
2024-10-28 11:01:48 +0500

    Install remote task to force reboot

---
+++ kompas/restart/restart.ps1
@@ -0,0 +1,14 @@
+#
+# Force restart remote computer
+#
+
+$who = 'Win10-3'
+
+Invoke-Command -ComputerName $who -ScriptBlock {
+  $Action = New-ScheduledTaskAction -Execute "powershell" -Argument "-c 'Restart-Computer -Force'"
+  $Trigger = New-ScheduledTaskTrigger -Once -At 01:23
+  $Settings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter 00:01:00
+  $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
+  $Task.Triggers[0].EndBoundary = (Get-Date).AddMinutes(100).ToString('s')
+  Register-ScheduledTask -TaskName "Restart!" -TaskPath uxm -InputObject $Task -User "System" -Force | Out-Null
+}
