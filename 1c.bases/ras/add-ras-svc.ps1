3f5f5083b093fb58c51b2022bc07eea78b9b92db
Stas Ukolov <ukoloff@gmail.com>
2024-10-29 14:13:51 +0500

    Run single RAS Service
    Couldn't start two :-(

--- 1c.bases/ras/add-ras-svc.ps1
+++ 1c.bases/ras/add-ras-svc.ps1
@@ -1,28 +1,20 @@
 #
-# Add 1C RAS service(s)
+# Add 1C RAS service
 #
 $portRAS = 1545
 $name = "1C.RAS"
 
-$hosts = @(3, 2)
-$server = "srv1c-ekbh"
 $port1c = 1640
 
 $exe = "c:\Program Files\1cv8\8.3.25.1336\bin\ras.exe"
-$cmd = "`"$exe`" --service cluster"
 $dsq = "1C:Enterprise 8.3 RAS Server"
 
-foreach ($id in $hosts) {
-  $aName = "$name-$id"
-  $aServer = $server + $id
-  sc.exe stop $aName    | Out-Null
-  sc.exe delete $aName  | Out-Null
-  $Service = @{
-    Name = "$name-$id"
-    BinaryPathName = "$cmd  -p $portRAS $aServer`:$port1c"
-    Description = "$dsq @$aServer"
-    StartupType = "Automatic"
-  }
-  $portRAS++
-  New-Service @Service
+sc.exe stop $name    | Out-Null
+sc.exe delete $name  | Out-Null
+$Service = @{
+  Name           = $name
+  BinaryPathName = "`"$exe`" --service cluster -p $portRAS 127.0.0.1:$port1c"
+  Description    = $dsq
+  StartupType    = "Automatic"
 }
+New-Service @Service
