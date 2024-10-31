ac074af8e5ba1b19e8d3828ff363f1081899f3d1
Stas Ukolov <ukoloff@gmail.com>
2024-10-31 08:55:50 +0500

    Check from list

--- kompas/before-update.ps1
+++ kompas/before-update.ps1
@@ -2,32 +2,34 @@
 # Проверка хоста на готовность (ручного) обновления
 #
 
-$who = 'uxm00743'
-# $who = 'uxm00437'
+$me = Split-Path $PSCommandPath -Parent
+$src = Join-Path $me candidates.txt
+$hosts = Get-Content $src | Sort-Object
 
-Write-Output "Testing: $who"
+foreach ($who in $hosts) {
+  Write-Output "Testing: $who"
 
-$online = Test-Connection $who -Quiet -Count 1
+  $online = Test-Connection $who -Quiet -Count 1
 
-if (!$online) {
-  Write-Output "Status: offline"
-  exit
+  if (!$online) {
+    Write-Output "Status: offline"
+    continue
+  }
+
+  $ver = Invoke-Command -ComputerName $who -ScriptBlock {
+    Get-ItemPropertyValue HKLM:\SOFTWARE\ASCON\KOMPAS-3D\21 -Name ProductVersion }
+
+  Write-Output "Kompas version: $ver"
+
+  [array]$run = Get-CimInstance -ComputerName $who -ClassName Win32_Process -Filter "Name Like 'kompas%' Or Name Like 'msiexec%'"
+  Write-Output "Running process(es): $($run.count)"
+  $run
+  Write-Output ""
+
+  if ($run.count -gt 0 -or !($ver.StartsWith('21.0.0.'))) {
+    continue
+  }
+
+  Write-Output "Installing..."
+  psexec -s "\\$who" msiexec /update \\service\Soft\kompasspatch\KOMPAS-3D_v21.0.26_x64.msp /quiet
 }
-
-$ver = Invoke-Command -ComputerName $who -ScriptBlock {
-  Get-ItemPropertyValue HKLM:\SOFTWARE\ASCON\KOMPAS-3D\21 -Name ProductVersion }
-
-Write-Output "Kompas version: $ver"
-
-[array]$run = Get-CimInstance -ComputerName $who -ClassName Win32_Process -Filter "Name Like 'kompas%' Or Name Like 'msiexec%'"
-Write-Output "Running process(es): $($run.count)"
-$run
-Write-Output ""
-
-if ($run.count -gt 0 -or !($ver.StartsWith('21.0.0.'))) {
-  Write-Output "Exiting"
-  exit
-}
-
-Write-Output "Installing..."
-psexec -s "\\$who" msiexec /update \\service\Soft\kompasspatch\KOMPAS-3D_v21.0.26_x64.msp /quiet
