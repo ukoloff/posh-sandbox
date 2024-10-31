87073042cc89f1ff053ff52940f1ae31b8e392a5
Stas Ukolov <ukoloff@gmail.com>
2024-10-31 08:29:47 +0500

    Check prerequisits

--- kompas/before-update.ps1
+++ kompas/before-update.ps1
@@ -2,6 +2,29 @@
 # Проверка хоста на готовность (ручного) обновления
 #
 
+# $who = 'uxm00926'
 $who = 'uxm00437'
 
 Write-Output "Testing: $who"
+
+$online = Test-Connection $who -Quiet -Count 1
+
+if (!$online) {
+  Write-Output "Status: offline"
+  exit
+}
+
+$ver = Invoke-Command -ComputerName $who -ScriptBlock {
+  Get-ItemPropertyValue HKLM:\SOFTWARE\ASCON\KOMPAS-3D\21 -Name ProductVersion }
+
+Write-Output "Kompas version: $ver"
+
+[array]$run = Get-CimInstance -ComputerName $who -ClassName Win32_Process -Filter "Name Like 'kompas%' Or Name Like 'msiexec%'"
+Write-Output "Running process(es): $($run.count)"
+$run
+Write-Output ""
+
+if ($run.count -gt 0 -or !($ver.StartsWith('21.0.0'))) {
+  Write-Output "Exiting"
+  exit
+}
