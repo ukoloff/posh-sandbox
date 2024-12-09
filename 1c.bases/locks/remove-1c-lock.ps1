d3e4e0ebc3b701a1ecdffa68e78f9c63ab540a35
Stas Ukolov <ukoloff@gmail.com>
2024-12-09 08:46:38 +0500

    Count from creation

--- 1c.bases/locks/remove-1c-lock.ps1
+++ 1c.bases/locks/remove-1c-lock.ps1
@@ -3,5 +3,5 @@ $d = Get-Date
 $d = $d.AddMinutes(-1)
 
 Get-ChildItem -LiteralPath $env:TEMP -Filter 1c_*.lock |
-Where-Object { $_.LastAccessTime -le $d } |
+Where-Object { $_.CreationTime -le $d } |
 Remove-Item
