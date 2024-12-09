dcba9cdec8ac33a239c176def88711aaa25647db
Stas Ukolov <ukoloff@gmail.com>
2024-12-09 08:43:44 +0500

    Do remove stale lock(s)

--- 1c.bases/locks/remove-1c-lock.ps1
+++ 1c.bases/locks/remove-1c-lock.ps1
@@ -3,4 +3,5 @@ $d = Get-Date
 $d = $d.AddMinutes(-1)
 
 Get-ChildItem -LiteralPath $env:TEMP -Filter 1c_*.lock |
-Where-Object { $_.LastAccessTime -le $d }
+Where-Object { $_.LastAccessTime -le $d } |
+Remove-Item
