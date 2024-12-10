a7c1e48ba81b48b35b5d6c5c73820ade5215805d
Stas Ukolov <ukoloff@gmail.com>
2024-12-10 09:20:47 +0500

    List missing persons

--- Sigur/emp-sync/sigir-emp-sync.ps1
+++ Sigur/emp-sync/sigir-emp-sync.ps1
@@ -3,7 +3,28 @@
 #
 
 $Server = 'srvskud-ekbh1-d'
+$src = "\\omzglobal.com\uxm\Exchange\employee_changes.csv"
 
 $cred = Get-StoredCredential -Target 'mysql:SKUD'
 
 Open-MySqlConnection -Server $Server -Port 3305 -Database  tc-db-main -Credential $cred
+
+$Users = Import-Csv  $src -Delimiter ";" -Encoding UTF8
+
+$Found = 0
+foreach ($user in $Users) {
+  $n = Invoke-SqlScalar @"
+    Select Count(*)
+    From personal
+    Where
+      NAME = @name
+      And
+      EXTID is NULL
+"@ -Parameters @{name = $user.displayName.Trim() }
+  if ($n -eq 1) {
+    $Found++
+    continue
+  }
+  Write-Output "$($user.displayName)`t$($user.employeeID)`t$n"
+}
+Write-Output "Found: $Found"
