5624b4642ca29893eac71cfe5c17b95fd6e5e9db
Stas Ukolov <ukoloff@gmail.com>
2024-12-10 09:29:09 +0500

    Search by employeeID

--- Sigur/emp-sync/sigir-emp-sync.ps1
+++ Sigur/emp-sync/sigir-emp-sync.ps1
@@ -12,6 +12,7 @@ Open-MySqlConnection -Server $Server -Port 3305 -Database  tc-db-main -Credentia
 $Users = Import-Csv  $src -Delimiter ";" -Encoding UTF8
 
 $Found = 0
+$NotFound = 0
 foreach ($user in $Users) {
   if ($user.company.Trim() -ne 'УЗХМ') { continue }
   $n = Invoke-SqlScalar @"
@@ -26,6 +27,30 @@ foreach ($user in $Users) {
     $Found++
     continue
   }
+  $NotFound++
   Write-Output "$($user.displayName)`t$($user.employeeID)`t$n"
 }
 Write-Output "Found: $Found"
+Write-Output "Not found: $NotFound"
+
+$Found = 0
+$NotFound = 0
+foreach ($user in $Users) {
+  if ($user.company.Trim() -ne 'УЗХМ') { continue }
+  $n = Invoke-SqlScalar @"
+    Select Count(*)
+    From personal
+    Where
+      TABID = @tab
+      And
+      EXTID is NULL
+"@ -Parameters @{tab = $user.employeeID.Trim() }
+  if ($n -eq 1) {
+    $Found++
+    continue
+  }
+  $NotFound++
+  Write-Output "$($user.displayName)`t$($user.employeeID)`t$n"
+}
+Write-Output "Found: $Found"
+Write-Output "Not found: $NotFound"
