edae698598a71eeb6f2abf8566ffcc2080fb064d
Stas Ukolov <ukoloff@gmail.com>
2024-12-18 13:48:54 +0500

    Create log file

--- wsus/monthly/wsus-install.ps1
+++ wsus/monthly/wsus-install.ps1
@@ -4,5 +4,12 @@
 $d = Get-Date
 if ([int]$d.DayOfWeek -ne 6 -or [Math]::Truncate(($d.Day + 6) / 7) -ne 3) {
   # НЕ 3-я суббота
-  exit
+  # exit
 }
+
+$dir = Split-Path $PSCommandPath -Parent
+$log = Join-Path $dir Logs
+$log = Join-Path $log $d.ToString("yyyy-MM-dd")
+$log = New-Item $log -Force -ItemType Directory
+$log = Join-Path $log "$($env:COMPUTERNAME)-$($d.ToString("HH-mm-ss_fff")).log"
+echo $log
