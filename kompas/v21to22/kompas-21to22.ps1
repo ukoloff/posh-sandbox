4ef0c5046f1cb4f88f7eb25bfdb5f84ea34715f5
Stas Ukolov <ukoloff@gmail.com>
2024-12-20 15:25:38 +0500

    Copy Licensing config

--- kompas/v21to22/kompas-21to22.ps1
+++ kompas/v21to22/kompas-21to22.ps1
@@ -1,6 +1,7 @@
 #
 # Обновление Компас с v21 на v22
 #
+$src = '\\omzglobal.com\uxm\Distribute\kompas\KOMPAS-3D_v22_x64\'
 
 if (!(Test-Path HKLM:\SOFTWARE\ASCON\KOMPAS-3D\21)) {
   exit
@@ -10,3 +11,6 @@ $kompas21 = '{05AB476A-CCCF-456F-B37F-43DDD7AE5F72}'
 
 msiexec /X $kompas21 /quiet
 
+$lic = Join-Path $src license.ini
+$licDst = Join-Path $env:ProgramData ASCON
+Copy-Item $lic $licDst -Force
