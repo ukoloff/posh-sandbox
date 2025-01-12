2319a9bee2c281c36a73d21cf2117682671123a5
Stas Ukolov <ukoloff@gmail.com>
2025-01-12 20:55:25 +0500

    Install patch

--- kompas/v21to22/polynom22.ps1
+++ kompas/v21to22/polynom22.ps1
@@ -4,7 +4,7 @@
 $src = '\\omzglobal.com\uxm\Distribute\kompas\KOMPAS-3D_v22_x64\'
 
 if (!(Test-Path HKLM:\SOFTWARE\ASCON\KOMPAS-3D\22)) {
-  # exit
+  exit
 }
 
 $Log = Join-Path $src Logs/p
@@ -20,6 +20,13 @@ function doPolynom {
     Write-Output "$(timeStamp)Installing:`t$($exe.FullName)"
     & $exe.FullName /passive /norestart | Write-Verbose
   }
+
+  foreach ($msp in Get-ChildItem $src -Recurse Polynom*.msp) {
+    Write-Output "$(timeStamp)Pathching:`t$($msp.FullName)"
+    msiexec /update $msp.FullName /passive /norestart | Write-Verbose
+  }
+
+  Write-Output "$(timeStamp)That's all folks!"
 }
 
 doPolynom >$Log 2>&1
