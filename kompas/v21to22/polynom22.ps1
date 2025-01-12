552bc39898d91070e7277b2cd11cb2761f5ce0c6
Stas Ukolov <ukoloff@gmail.com>
2025-01-12 20:38:40 +0500

    Start Polynom patch

---
+++ kompas/v21to22/polynom22.ps1
@@ -0,0 +1,9 @@
+﻿#
+# Добавка к Компас 22, чтобы работал Polynom
+#
+$src = '\\omzglobal.com\uxm\Distribute\kompas\KOMPAS-3D_v22_x64\'
+
+if (!(Test-Path HKLM:\SOFTWARE\ASCON\KOMPAS-3D\22)) {
+  # exit
+}
+
