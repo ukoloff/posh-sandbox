d85f995c69fd0eedd8bb1871c34c4bd5384ece1f
Stas Ukolov <ukoloff@gmail.com>
2025-01-30 11:12:16 +0500

    Do associate

--- link/7z/7z-assoc.ps1
+++ link/7z/7z-assoc.ps1
@@ -1,6 +1,9 @@
 #
-# Associate 7z file maneger with types
+# Associate 7z file manager with types
 #
+$exts = -split "7z zip rar cab xz txz lzma tar cpio bz2 bzip2 tbz2 gz gzip tgz z lzh lha rpm deb arj"
+$handler = '7z.uxm'
+$to = 'Registry::HKCR'
 
 $cfg = 'HKLM:\SOFTWARE\7-Zip'
 
@@ -13,3 +16,7 @@ if (!(Test-Path $path -PathType Container)) {
   exit
 }
 
+New-Item -path "$to\$handler\shell\open\command" -force -value "`"$path\7zFM.exe`" `"%1`""
+foreach ($ext in $exts) {
+  New-Item -path "$to\.$ext" -force -value $handler
+}
