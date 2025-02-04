6373b0ddfd121328bad15f1e16ceeadefaf3ca5f
Stas Ukolov <ukoloff@gmail.com>
2025-02-04 10:18:41 +0500

    Apply scheme

--- 1c2AD/welcome/welcome-letters.ps1
+++ 1c2AD/welcome/welcome-letters.ps1
@@ -3,12 +3,17 @@
 #
 $newEmployees = '\\omzglobal.com\uxm\Exchange\employee_mail.csv'
 
-$db = Split-Path $PSCommandPath -Parent
-$db = Join-Path $db .db
+$db = Join-Path (Split-Path $PSCommandPath -Parent) .db
 $db = New-Item $db -Force -ItemType Directory
 $db = Join-Path $db welcome.sq3
-echo ""> $db
+Open-SQLiteConnection $db
+if ((Get-Item $db).Length -eq 0) {
+  $sql = Get-Content -Raw (Join-Path (Split-Path $PSCommandPath -Parent) ddl.sql)
+  Invoke-SqlUpdate $sql
+}
 
 foreach ($user in Import-Csv $newEmployees -Delimiter ';' -Encoding UTF8) {
   $user.sAMAccountName
 }
+
+Close-SqlConnection
