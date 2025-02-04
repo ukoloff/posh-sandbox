52e592c9ec69065ff57e6eb7586abb410d4ff66d
Stas Ukolov <ukoloff@gmail.com>
2025-02-04 10:02:24 +0500

    Create DB folder

--- 1c2AD/welcome/welcome-letters.ps1
+++ 1c2AD/welcome/welcome-letters.ps1
@@ -3,6 +3,12 @@
 #
 $newEmployees = '\\omzglobal.com\uxm\Exchange\employee_mail.csv'
 
+$db = Split-Path $PSCommandPath -Parent
+$db = Join-Path $db .db
+$db = New-Item $db -Force -ItemType Directory
+$db = Join-Path $db welcome.sq3
+echo ""> $db
+
 foreach ($user in Import-Csv $newEmployees -Delimiter ';' -Encoding UTF8) {
   $user.sAMAccountName
 }
