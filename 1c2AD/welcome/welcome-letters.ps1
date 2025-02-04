2decfd1335115279c1df2826785f2e60d7514797
Stas Ukolov <ukoloff@gmail.com>
2025-02-04 13:34:38 +0500

    Mark old users

--- 1c2AD/welcome/welcome-letters.ps1
+++ 1c2AD/welcome/welcome-letters.ps1
@@ -7,7 +7,8 @@ $db = Join-Path (Split-Path $PSCommandPath -Parent) .db
 $db = New-Item $db -Force -ItemType Directory
 $db = Join-Path $db welcome.sq3
 Open-SQLiteConnection $db
-if ((Get-Item $db).Length -eq 0) {
+$isNew = (Get-Item $db).Length -eq 0
+if ($isNew) {
   $sql = Get-Content -Raw (Join-Path (Split-Path $PSCommandPath -Parent) ddl.sql)
   $n = Invoke-SqlUpdate $sql
 }
@@ -19,5 +20,10 @@ foreach ($user in Import-Csv $newEmployees -Delimiter ';' -Encoding UTF8) {
     Values(@u)
 "@ -Parameters @{u=$user.sAMAccountName}
 }
-
+if ($isNew) {
+  $n = Invoke-SqlUpdate @"
+    Update users
+    Set ztime = CURRENT_TIMESTAMP
+"@
+}
 Close-SqlConnection
