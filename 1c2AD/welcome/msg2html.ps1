1483f226e7887b8396a7d42919ae5f076fd344a3
Stas Ukolov <ukoloff@gmail.com>
2025-02-05 15:14:04 +0500

    Avoid freeze

--- 1c2AD/welcome/msg2html.ps1
+++ 1c2AD/welcome/msg2html.ps1
@@ -1,10 +1,12 @@
 #
 # Преобразовать сообщение Outlook .msg -> .html + вложения
 #
-$msg = Join-Path (Split-Path $PSCommandPath -Parent) msg\uxm.msg
 
-$outlook = New-Object -comobject outlook.application
+# $msg = Join-Path (Split-Path $PSCommandPath -Parent) msg\uxm.msg
+$msg = 'C:\Users\s.ukolov\Documents\repo\posh-sandbox\1c2AD\welcome\msg\uxm.msg'
+
+$o = New-Object -ComObject Outlook.Application
 echo $msg
-$z = $outlook.Session.OpenSharedItem($msg)
+$z = $o.Session.OpenSharedItem($msg)
 
 echo $z.body
