8e4e99560d77b64a9df7fc5a3bb9e7138675eacb
Stas Ukolov <ukoloff@gmail.com>
2025-02-05 16:27:20 +0500

    Format

--- 1c2AD/welcome/body/msg2html.ps1
+++ 1c2AD/welcome/body/msg2html.ps1
@@ -5,7 +5,6 @@
 $folder = Join-Path (Split-Path $PSCommandPath -Parent) ..\msg
 
 Add-Type -AssemblyName System.Windows.Forms
-
 $d = New-Object System.Windows.Forms.OpenFileDialog
 $d.Title = "Выберите почтовое сообщение"
 $d.Filter = 'Почтовые сообщения|*.msg|Все файлы|*.*'
@@ -16,21 +15,28 @@ if ($d.ShowDialog() -ne "OK") {
 }
 $src = $d.FileName
 
-# $msg = 'C:\Users\s.ukolov\Documents\repo\posh-sandbox\1c2AD\welcome\msg\uxm.msg'
-
 $o = New-Object -ComObject Outlook.Application
 $z = $o.Session.OpenSharedItem($src)
 
 $name = (Get-Item $src).BaseName
 $dst = New-Item -Force -ItemType Directory (Join-Path $folder $name)
 # Move-Item $src $dst
-$z.Subject | Out-File (Join-Path $dst subject.txt) -Encoding utf8
-$z.HTMLbody | Out-File (Join-Path $dst body.html) -Encoding utf8
-$z.body | Out-File (Join-Path $dst body.txt) -Encoding utf8
-'*' | Out-File (Join-Path $dst .gitignore) -Encoding utf8
+
+$z.Subject |
+Out-File (Join-Path $dst subject.txt) -Encoding utf8
+$z.HTMLbody |
+Out-File (Join-Path $dst body.html) -Encoding utf8
+$z.body |
+Out-File (Join-Path $dst body.txt) -Encoding utf8
+'*' |
+Out-File (Join-Path $dst .gitignore) -Encoding utf8
+
+# Attachments
 foreach ($a in $z.Attachments) {
   $aFolder = New-Item -Force -ItemType Directory (Join-Path $dst files)
   $a.SaveAsFile((Join-Path $aFolder $a.FileName))
 }
+
+# Cleanup (???)
 $z.Close(1) # olDiscard
 # $o.Quit()
