f450b8ff0d40e868cef6f8ee90299f4ed5bbf95e
Stas Ukolov <ukoloff@gmail.com>
2025-02-06 08:58:27 +0500

    Clean cid: URLs for attachments

--- 1c2AD/welcome/body/msg2html.ps1
+++ 1c2AD/welcome/body/msg2html.ps1
@@ -24,17 +24,28 @@ $dst = New-Item -Force -ItemType Directory (Join-Path $folder $name)
 
 $z.Subject |
 Out-File (Join-Path $dst subject.txt) -Encoding utf8
-$z.HTMLbody |
+($body = $z.HTMLbody) |
 Out-File (Join-Path $dst body.html) -Encoding utf8
 $z.body |
 Out-File (Join-Path $dst body.txt) -Encoding utf8
 '*' |
 Out-File (Join-Path $dst .gitignore) -Encoding utf8
 
+$changed = 0
 # Attachments
 foreach ($a in $z.Attachments) {
   $aFolder = New-Item -Force -ItemType Directory (Join-Path $dst files)
-  $a.SaveAsFile((Join-Path $aFolder $a.FileName))
+  $aName = Join-Path $aFolder $a.FileName
+  $a.SaveAsFile($aName)
+  $re = [regex]::Escape("cid:$($a.FileName)@") + "[a-z\d]{2,}[.][a-z\d]{2,}"
+  $rep = "cid:$($a.FileName)"
+  $body = $body -replace $re, $rep
+  $changed++
+}
+
+if ($changed) {
+  $body |
+  Out-File (Join-Path $dst body.html) -Encoding utf8
 }
 
 # Cleanup (???)
