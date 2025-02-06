76c62342b0fe92ce00372f315c851a7774387e7a
Stas Ukolov <ukoloff@gmail.com>
2025-02-06 09:15:40 +0500

    Change initial folder

--- 1c2AD/welcome/body/msg2html.ps1
+++ 1c2AD/welcome/body/msg2html.ps1
@@ -2,7 +2,7 @@
 # Преобразовать сообщение Outlook .msg -> .html + вложения
 #
 
-$folder = Join-Path (Split-Path $PSCommandPath -Parent) ..\msg
+$folder = Join-Path (Split-Path $PSCommandPath -Parent) .
 
 Add-Type -AssemblyName System.Windows.Forms
 $d = New-Object System.Windows.Forms.OpenFileDialog
