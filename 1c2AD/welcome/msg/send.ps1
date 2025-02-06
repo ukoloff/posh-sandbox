33830734057ddb27bf39d57c2ce5d9d1c3874f89
Stas Ukolov <ukoloff@gmail.com>
2025-02-06 09:05:52 +0500

    Send all available .msg

--- 1c2AD/welcome/msg/send.ps1
+++ 1c2AD/welcome/msg/send.ps1
@@ -1,28 +1,30 @@
 #
 # Send mail with attachments
 #
-$name = 'uxm'
-
-$src = Join-Path (Split-Path $PSCommandPath -Parent) $name
-
-$subject = (Get-Content -Raw (Join-Path $src subject.txt)).Trim()
-$body = (Get-Content -Raw (Join-Path $src body.html)).Trim()
-[array]$az = Get-ChildItem (Join-Path $src files) | ForEach-Object { $_.FullName }
-
 $cred = Get-StoredCredential -Target serviceuxm@omzglobal.com
 
-$mail = @{
-  BodyAsHtml  = $true
-  Body        = $body
-  Subject     = $subject
-  Attachments = $az
-  From        = 'serviceuxm@omzglobal.com'
-  To          = 'Stanislav.Ukolov@omzglobal.com'
-  SmtpServer  = 'srvmail-ekbh5.omzglobal.com'
-  Port        = '2525'
-  Encoding    = 'UTF8'
-  Credential  = $cred
+Split-Path $PSCommandPath -Parent |
+Get-ChildItem -Directory |
+ForEach-Object {
+  $src = $_.FullName
+  $subject = (Get-Content -Raw (Join-Path $src subject.txt)).Trim()
+  $body = (Get-Content -Raw (Join-Path $src body.html)).Trim()
+  [array]$az = Join-Path $src files |
+  Get-ChildItem -File |
+  ForEach-Object { $_.FullName }
+
+  $mail = @{
+    BodyAsHtml  = $true
+    Body        = $body
+    Subject     = $subject
+    Attachments = $az
+    From        = 'serviceuxm@omzglobal.com'
+    To          = 'Stanislav.Ukolov@omzglobal.com'
+    SmtpServer  = 'srvmail-ekbh5.omzglobal.com'
+    Port        = '2525'
+    Encoding    = 'UTF8'
+    Credential  = $cred
+  }
+
+  Send-MailMessage @mail
 }
-
-Send-MailMessage @mail
-
