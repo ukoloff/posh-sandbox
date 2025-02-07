54c04bc277ab04840b9deb99d877be2dc943d652
Stas Ukolov <ukoloff@gmail.com>
2025-02-07 14:30:12 +0500

    Move stale files

--- 2trash/kzk/clean-messer.ps1
+++ 2trash/kzk/clean-messer.ps1
@@ -1,4 +1,17 @@
 ﻿#
 # Очистка папки \\kzkServ\Data\ZEX23\MESSER от старых файлов
 #
-$folder = '\\kzkServ\Data\ZEX23\MESSER'
+$folder = 'e:\Data\ZEX23\MESSER'
+$day = 14
+if (!(Test-Path $folder -PathType Container)) {
+  $folder = 'C:\temp\messer\trash'
+}
+
+$d = (Get-Date).AddDays(-$day)
+Get-ChildItem -Path $folder -File |
+Where-Object { $_.LastWriteTime -le $d } |
+ForEach-Object {
+  $dst = Join-Path "$folder.sav" $_.LastAccessTime.ToString("yyyy\\MM\\")
+  $dst = New-Item $dst -Force -ItemType Directory
+  Move-Item $_.FullName $dst.FullName -Force
+}
