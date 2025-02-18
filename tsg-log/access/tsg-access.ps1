d079bd04119456296ea419b10d0c6f6d4de7c50a
Stas Ukolov <ukoloff@gmail.com>
2025-02-18 10:17:05 +0500

    Dump sessions

--- tsg-log/access/tsg-access.ps1
+++ tsg-log/access/tsg-access.ps1
@@ -3,12 +3,36 @@
 #
 $Groups = Write-Output  RDP RDP_UMZ Support RDP-enable-resourse
 
-foreach ($g in $Groups) {
-  (Get-ADGroup $g -Properties member).member |
-  ForEach-Object {
-    Get-ADUser $_ -Properties CanonicalName, Title |
-    Select-Object Name, CanonicalName, Enabled, SamAccountName, Title |
-    Sort-Object Name
-  } |
-  Export-Excel -WorksheetName $g
-}
+# foreach ($g in $Groups) {
+#   (Get-ADGroup $g -Properties member).member |
+#   ForEach-Object {
+#     Get-ADUser $_ -Properties CanonicalName, Title |
+#     Select-Object Name, CanonicalName, Enabled, SamAccountName, Title |
+#     Sort-Object Name
+#   } |
+#   Export-Excel -WorksheetName $g
+# }
+
+$cred = Get-StoredCredential -Target pqsql:TSG  # Fallback to Kerberos if not found
+Open-PostGreConnection -Server 'pg.ekb.ru' -Database uxm -Credential $cred
+Invoke-SqlQuery @"
+select
+	"user",
+	date_trunc('month', start)::date as "month",
+	count(*) as "count",
+	count(distinct date_part('day', start)) as days,
+	sum(duration) as "seconds",
+	sum(inb) as "inBytes",
+	sum(outb) as "outBytes"
+from
+	tsg
+where
+	start >= '2025-01-01'
+group by
+	"user",
+	date_trunc('month', start)::date
+order by
+	1, 2
+"@ |
+Export-Excel -WorksheetName Sessions
+Close-SqlConnection
