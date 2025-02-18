3fcde696858e4c1e4f4be7846b050625f6bef070
Stas Ukolov <ukoloff@gmail.com>
2025-02-18 10:05:04 +0500

    Dump group membership

--- tsg-log/access/tsg-access.ps1
+++ tsg-log/access/tsg-access.ps1
@@ -1,6 +1,14 @@
 #
 # Enumerate access to TS Gateway
 #
-$Groups= Write-Output  RDP RDP_UMZ Support RDP-enable-resourse
+$Groups = Write-Output  RDP RDP_UMZ Support RDP-enable-resourse
 
-$Groups
+foreach ($g in $Groups) {
+  (Get-ADGroup $g -Properties member).member |
+  ForEach-Object {
+    Get-ADUser $_ -Properties CanonicalName, Title |
+    Select-Object Name, CanonicalName, Enabled, SamAccountName, Title |
+    Sort-Object Name
+  } |
+  Export-Excel -WorksheetName $g
+}
