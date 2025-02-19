a0ce36394c7334cd61cbb62018db4c52dca29223
Stas Ukolov <ukoloff@gmail.com>
2025-02-19 10:07:18 +0500

    Log IP

--- link/ocs/ocs-patch.ps1
+++ link/ocs/ocs-patch.ps1
@@ -11,6 +11,7 @@ function timeStamp() {
 
 function sysInfo {
   "$(timeStamp)Host:`t$($env:COMPUTERNAME)"
+  "$(timeStamp)IP:`t$(-join ((Get-NetIPAddress -AddressFamily IPv4).IPAddress | ? { $_ -match "^10[.]"}))"
   @"
 $(timeStamp)Users:`t$((
     Get-Process -IncludeUserName |
