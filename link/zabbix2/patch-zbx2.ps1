97698b4dd96a69b2a75dd547410222b8595b857e
Stas Ukolov <ukoloff@gmail.com>
2025-02-24 13:21:56 +0500

    Start Zabbix2 patch

---
+++ link/zabbix2/patch-zbx2.ps1
@@ -0,0 +1,6 @@
+#
+# Patch Zabbix Agent2 config to replace IP with DNS
+#
+$Logs = '\\service\Soft\Scripts\Logs\Zabbix2'
+
+
---
+++ link/zabbix2/readme.md
@@ -0,0 +1,3 @@
+# Zabbix Agent2
+
+Замена IP-адреса в конфиге Zabbix Agent2 на IP адрес
