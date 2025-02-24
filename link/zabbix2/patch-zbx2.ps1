55c3681d4f5a6a1fdf260e09992f3305d3bd970b
Stas Ukolov <ukoloff@gmail.com>
2025-02-24 13:23:45 +0500

    Start logging

--- link/zabbix2/patch-zbx2.ps1
+++ link/zabbix2/patch-zbx2.ps1
@@ -3,4 +3,24 @@
 #
 $Logs = '\\service\Soft\Scripts\Logs\Zabbix2'
 
+function timeStamp() {
+  "[$(Get-Date -UFormat '%Y-%m-%d %T %Z')]`t"
+}
 
+function sysInfo {
+  "$(timeStamp)Host:`t$($env:COMPUTERNAME)"
+  "$(timeStamp)IP:`t$(-join ((Get-NetIPAddress -AddressFamily IPv4).IPAddress | ? { $_ -match "^10[.]|^192[.]168[.]"}))"
+  @"
+$(timeStamp)Users:`t$((
+    Get-Process -IncludeUserName |
+    Select-Object -ExpandProperty UserName -Unique |
+    Where-Object {$_ -notmatch ' '} |
+    Sort-Object) -join ' ')
+"@
+}
+
+function patchZabbix2 {
+  sysInfo
+}
+
+patchZabbix2
