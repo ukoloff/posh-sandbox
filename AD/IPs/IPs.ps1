61ff2adb20c1c900abc251d66a488c46f639e819
Stas Ukolov <ukoloff@gmail.com>
2025-02-28 14:55:14 +0500

    Build Set of IP

--- AD/IPs/IPs.ps1
+++ AD/IPs/IPs.ps1
@@ -1,16 +1,14 @@
 #
 # Get IP for Domain Computers
 #
-$OUs = @(
+[System.Collections.Generic.HashSet[string]]$IPs = @(
   'OU=EKBH,OU=uxm,OU=MS,DC=omzglobal,DC=com'
   'OU=servers-ekb-uhm,OU=Servers,DC=omzglobal,DC=com'
-)
-
-$OUs |
+) |
 ForEach-Object { Get-ADComputer -Filter * -SearchBase $_ } |
 Select-Object -ExpandProperty DNSHostName |
 Where-Object { $_ } |
 ForEach-Object { Resolve-DNSName $_ -Type A } |
-Select-Object -ExpandProperty IPAddress |
-Sort-Object |
-Out-GridView
+Select-Object -ExpandProperty IPAddress
+
+$IPs
