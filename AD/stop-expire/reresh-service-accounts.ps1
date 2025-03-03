b424d80966d5866e9155113020179af682c029df
Stas Ukolov <ukoloff@gmail.com>
2025-03-03 11:50:11 +0500

    Reset password time

--- AD/stop-expire/reresh-service-accounts.ps1
+++ AD/stop-expire/reresh-service-accounts.ps1
@@ -4,4 +4,6 @@
 $OU = 'OU=ekb.ru,OU=Service,OU=EKBH,OU=uxm,OU=MS,DC=omzglobal,DC=com'
 
 Get-ADUser -SearchBase $OU -Filter * |
-Out-GridView
+Set-ADUser -Replace @{pwdLastSet = 0 } -PassThru |
+Set-ADUser -Replace @{pwdLastSet = -1 } -PassThru |
+Get-ADUser -Properties pwdLastSet, PasswordLastSet
