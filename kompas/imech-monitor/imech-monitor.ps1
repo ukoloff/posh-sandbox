42214a8c33a72c61b4265cafa63aa737920f2823
Stas Ukolov <ukoloff@gmail.com>
2025-03-05 08:33:29 +0500

    Parse values

--- kompas/imech-monitor/imech-monitor.ps1
+++ kompas/imech-monitor/imech-monitor.ps1
@@ -8,4 +8,18 @@ $q = Invoke-WebRequest $URL -UseBasicParsing
 $dom = New-Object -ComObject "HTMLFile"
 $dom.IHTMLDocument2_write([System.Text.Encoding]::Default.GetString($q.Content))
 
-$dom.all.tags('h2') | % innerText
+$dom.all.tags('h2') |
+% innerText |
+% {
+  switch -Regex ($_) {
+    ':\s*(?<Key>\d+)\s*$' {
+      "Key $($Matches.Key)"
+    }
+    '\s+(?<D>\d{1,2})([-./])(?<M>\d{1,2})\1(?<Y>\d{2,4})' {
+      "Date $($Matches.Y)-$($Matches.M)-$($Matches.D)"
+    }
+    '\s*(?<Sessions>\d+)\s+.*TCP/IP' {
+      "Sessions $($Matches.Sessions)"
+    }
+  }
+}
