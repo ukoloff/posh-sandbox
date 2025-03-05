b35dd92ab0f973566501947b3ed57235b6ae038b
Stas Ukolov <ukoloff@gmail.com>
2025-03-05 08:21:07 +0500

    Extract <h2> tags

--- kompas/imech-monitor/imech-monitor.ps1
+++ kompas/imech-monitor/imech-monitor.ps1
@@ -3,5 +3,9 @@
 #
 $URL = 'http://imech:8995/'
 
-$q = Invoke-WebRequest $URL
-$q
+$q = Invoke-WebRequest $URL -UseBasicParsing
+
+$dom = New-Object -ComObject "HTMLFile"
+$dom.IHTMLDocument2_write([System.Text.Encoding]::Default.GetString($q.Content))
+
+$dom.all.tags('h2') | % innerText
