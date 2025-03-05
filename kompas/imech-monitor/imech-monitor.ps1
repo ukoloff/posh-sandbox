122ce0fe5ce56043fde5b884c089af5349fb9324
Stas Ukolov <ukoloff@gmail.com>
2025-03-05 08:40:14 +0500

    Format as JSON

--- kompas/imech-monitor/imech-monitor.ps1
+++ kompas/imech-monitor/imech-monitor.ps1
@@ -8,18 +8,28 @@ $q = Invoke-WebRequest $URL -UseBasicParsing
 $dom = New-Object -ComObject "HTMLFile"
 $dom.IHTMLDocument2_write([System.Text.Encoding]::Default.GetString($q.Content))
 
+$out = [PSCustomObject]@{
+  key   = $null
+  date = $null
+  days = $null
+  count = $null
+}
+
 $dom.all.tags('h2') |
 % innerText |
 % {
   switch -Regex ($_) {
     ':\s*(?<Key>\d+)\s*$' {
-      "Key $($Matches.Key)"
+      $out.key = $Matches.Key
     }
     '\s+(?<D>\d{1,2})([-./])(?<M>\d{1,2})\1(?<Y>\d{2,4})' {
-      "Date $($Matches.Y)-$($Matches.M)-$($Matches.D)"
+      $out.date = "$($Matches.Y)-$($Matches.M)-$($Matches.D)"
+      $out.days = ((Get-Date $out.date) - (Get-Date)).Days
     }
     '\s*(?<Sessions>\d+)\s+.*TCP/IP' {
-      "Sessions $($Matches.Sessions)"
+      $out.count = [int]$Matches.Sessions
     }
   }
 }
+
+$out | ConvertTo-Json
