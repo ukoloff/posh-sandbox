4503d78b10abb96358fa89903759421d1d8c6dc4
Stas Ukolov <ukoloff@gmail.com>
2025-03-21 11:41:20 +0500

    List backups

--- mssql/restore/restore-tui.ps1
+++ mssql/restore/restore-tui.ps1
@@ -5,3 +5,27 @@
 $src = "SRVSQL-1C"
 $dst = "SRVSQL-1Ctests"
 $dstFolder = "D:\"
+
+function listBackups {
+  Invoke-SqlQuery @"
+    SELECT
+      database_name as [name],
+      min(backup_start_date) as dateA,
+      count(*) as [n],
+      max(backup_start_date) as dateZ
+    FROM
+      backupset
+    group by
+      database_name
+    order by
+      1
+"@ -ConnectionName src
+}
+
+Open-SQLConnection -ConnectionName src -Server $src -Database msdb
+Open-SQLConnection -ConnectionName dst -Server $dst -Database msdb
+
+listBackups
+
+Close-SqlConnection -ConnectionName src
+Close-SqlConnection -ConnectionName dst
