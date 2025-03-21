db1207d5a70d3dcd293f26e34eaa8b1cbdbe73b9
Stas Ukolov <ukoloff@gmail.com>
2025-03-21 12:00:05 +0500

    Select BD to restore

--- mssql/restore/restore-tui.ps1
+++ mssql/restore/restore-tui.ps1
@@ -21,10 +21,37 @@ function listBackups {
 "@ -ConnectionName src
 }
 
+function selectBD {
+  "Имеющиеся резервные копии на сервере: $src"
+  $backups = listBackups
+  $backups | ft
+  while ($true) {
+    $n = Read-Host "Выберите номер БД для восстановления"
+    $n = $n.Trim()
+    if ($n -eq '') {
+      exit
+    }
+    if ($n -notmatch '^\d+$') {
+        Write-Warning "Требуется число!"
+        continue
+    }
+    $n = [int]$n
+    if ($n -lt 1 -or $n -gt $backups.count) {
+      Write-Warning "Введите число от 1 до $($backups.count)"
+      continue
+    }
+    return $backups[$n-1].БД
+  }
+}
+
+function Run {
+  selectBD
+}
+
 Open-SQLConnection -ConnectionName src -Server $src -Database msdb
 Open-SQLConnection -ConnectionName dst -Server $dst -Database msdb
 
-listBackups | ft
+Run
 
 Close-SqlConnection -ConnectionName src
 Close-SqlConnection -ConnectionName dst
