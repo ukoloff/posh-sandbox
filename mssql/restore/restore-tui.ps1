f2a962d4a909b5dc4e022cd8141db52fb105405b
Stas Ukolov <ukoloff@gmail.com>
2025-03-21 11:43:18 +0500

    Numerate

--- mssql/restore/restore-tui.ps1
+++ mssql/restore/restore-tui.ps1
@@ -9,23 +9,22 @@ $dstFolder = "D:\"
 function listBackups {
   Invoke-SqlQuery @"
     SELECT
-      database_name as [name],
-      min(backup_start_date) as dateA,
-      count(*) as [n],
-      max(backup_start_date) as dateZ
+      ROW_NUMBER() over(order by database_name) as [№],
+      database_name as [БД],
+      min(backup_start_date) as [Первый],
+      count(*) as [Кол-во],
+      max(backup_start_date) as [Последний]
     FROM
       backupset
     group by
       database_name
-    order by
-      1
 "@ -ConnectionName src
 }
 
 Open-SQLConnection -ConnectionName src -Server $src -Database msdb
 Open-SQLConnection -ConnectionName dst -Server $dst -Database msdb
 
-listBackups
+listBackups | ft
 
 Close-SqlConnection -ConnectionName src
 Close-SqlConnection -ConnectionName dst
