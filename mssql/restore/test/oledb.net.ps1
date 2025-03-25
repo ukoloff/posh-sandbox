3db705ef0149d45032b478937ee7d9a4eb723aea
Stas Ukolov <ukoloff@gmail.com>
2025-03-25 10:06:09 +0500

    Use OleDB @Net

--- mssql/restore/test/oledb.net.ps1
+++ mssql/restore/test/oledb.net.ps1
@@ -6,4 +6,27 @@ $b.Provider = 'sqloledb'
 $b.Add('Integrated Security', 'SSPI')
 $b.Add('Data Source', "SRVSQL-1C")
 $b.Add('Initial Catalog', 'msdb')
-$b.ConnectionString
+
+$conn = New-Object System.Data.OleDb.OleDbConnection $b.ConnectionString
+$conn.Open()
+
+$cmd = $conn.CreateCommand()
+$cmd.CommandText = @"
+  Select
+    *
+  From
+    backupset
+  -- Where
+  --  database_name = @db
+"@
+# $cmd.Parameters.AddWithValue('@db', 'ERP_WORK')
+# $cmd.Parameters.Add('@db', [Data.SQLDBType]::varchar).Value = 'ERP_WORK'
+$r = $cmd.ExecuteReader()
+while ($r.Read()) {
+  $row = [ordered]@{}
+  foreach ($i in 1..$r.FieldCount) {
+    $row[$r.GetName($i - 1)] = $r.GetValue($i - 1)
+  }
+  [PSCustomObject]$row
+}
+
