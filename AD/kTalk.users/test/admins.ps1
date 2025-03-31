b20882f340b79cf7a65c17cc6bab7ca6e679aa6d
Stas Ukolov <ukoloff@gmail.com>
2025-03-31 11:04:12 +0500

    List admins

---
+++ AD/kTalk.users/test/admins.ps1
@@ -0,0 +1,19 @@
+#
+# User sync omzgloal.com -> uzxm.ktalk.ru
+#
+
+$cred = Get-StoredCredential -Target ktalk:uzxm
+
+# https://techibee.com/powershell/convert-system-security-securestring-to-plain-text-using-powershell/2599
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($cred.Password)
+$PlainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+$URI = "https://$($cred.UserName).ktalk.ru/api/Users"
+$Headers = @{
+  Headers = @{
+   'X-Auth-Token' = $PlainPassword
+  }
+}
+$q = Invoke-WebRequest -Uri "$URI/scan?includeDisabled=true&role=admin" @Headers
+$u = (ConvertFrom-Json $q.Content).users
+$u | Out-GridView
