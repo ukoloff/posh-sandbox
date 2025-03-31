06e37a268fe252351353ec3e22f632f3d69a0dfc
Stas Ukolov <ukoloff@gmail.com>
2025-03-31 10:33:10 +0500

    Decode password

--- AD/kTalk.users/ktalk-sync-users.ps1
+++ AD/kTalk.users/ktalk-sync-users.ps1
@@ -4,6 +4,14 @@
 
 $cred = Get-StoredCredential -Target ktalk:uzxm
 
-$URI = "https://$($cred.UserName).ktalk.ru/Users"
+# https://techibee.com/powershell/convert-system-security-securestring-to-plain-text-using-powershell/2599
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($cred.Password)
+$PlainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
-$URI
+$URI = "https://$($cred.UserName).ktalk.ru/Users"
+$Headers = @{
+  Headers = @{
+   'X-Auth-Token' = $PlainPassword
+  }
+}
+Invoke-WebRequest -Uri "$URI/scan" @Headers
