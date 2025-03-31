c19c29423c2ec78a6ac37da48d48bd5b28365d51
Stas Ukolov <ukoloff@gmail.com>
2025-03-31 10:58:58 +0500

    Try to update user

---
+++ AD/kTalk.users/test/update.ps1
@@ -0,0 +1,26 @@
+﻿#
+# User sync omzgloal.com -> uzxm.ktalk.ru
+#
+
+$cred = Get-StoredCredential -Target ktalk:uzxm
+
+# https://techibee.com/powershell/convert-system-security-securestring-to-plain-text-using-powershell/2599
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($cred.Password)
+$PlainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+$URI = "https://$($cred.UserName).ktalk.ru/api/Users"
+$Headers = @{
+  Headers = @{
+   'X-Auth-Token' = $PlainPassword
+   'Content-Type' = 'application/json'
+  }
+}
+
+$update = @(@{
+  userKey = 'bef471d4-fe11-4c14-9fa3-5fbaf68246c7'
+  post = 'Ведущий системный администратор'
+  department = 'Группа поддержки ИТ-инфракструктуры'
+})
+$q = $null
+$q = Invoke-WebRequest -Uri $URI @Headers -Method POST -Body (ConvertTo-Json -Compress $update)
+$q
