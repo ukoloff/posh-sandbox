e513c7cdc42cf7b99619c9d50ebc839569cbee88
Stas Ukolov <ukoloff@gmail.com>
2025-04-02 16:44:10 +0500

    Find user in AD

--- Sigur/adSync/ad-sigur-sync.ps1
+++ Sigur/adSync/ad-sigur-sync.ps1
@@ -4,8 +4,29 @@
 #
 $Server = 'srvskud-ekbh1-d'
 
+function listOperators {
+  $q = Invoke-SqlQuery @"
+    select
+      ID as id,
+      EXTID as guid,
+      USER_ENABLED as oper,
+      USER_T_SSPILOGIN as ad
+    from
+      personal
+    where
+      EXTID is not null
+      and type = 'EMP'
+"@
+  foreach ($z in $q) {
+    $ad = ([ADSISearcher]"(&(objectCategory=User)(objectGUID=$($z.guid -replace "(.{2})", '\$1')))").FindOne()
+    $ad.Path
+  }
+}
+
 $cred = Get-StoredCredential -Target 'mysql:SKUD'
 Open-MySqlConnection -Server $Server -Port 3305 -Database  tc-db-main -Credential $cred
+
+listOperators
 
 Close-SqlConnection
 
