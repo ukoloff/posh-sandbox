a2432b3bbdbc7f8c8c735071900d00f40fb5062b
Stas Ukolov <ukoloff@gmail.com>
2025-04-03 11:13:56 +0500

    Create domain folder

--- Sigur/adSync/ad-sigur-sync.ps1
+++ Sigur/adSync/ad-sigur-sync.ps1
@@ -27,8 +27,55 @@ function guessDomain($uid) {
       join addomains d on p.AD_DOMAIN_ID = d.ID
     where
       p.id = @id
-"@ -Parameters @{id = $uid}
+"@ -Parameters @{id = $uid }
   dbNull $q
+}
+
+$folderCache = @{}
+
+function findFolder($name) {
+  if (!$name) {
+    $name = '!domain.ad'
+  }
+  if ($folderCache[$name]) {
+    return $folderCache[$name]
+  }
+  $q = Invoke-SqlScalar @"
+    select
+      ID
+    from
+      personal p
+    where
+      name = @name
+      and `TYPE` = 'DEP'
+      and STATUS = 'AVAILABLE'
+      and not exists(select * from personal q where q.ID = p.PARENT_ID )
+    order by
+      CREATEDTIME
+    limit 1
+"@ -Parameters @{name = $name }
+  $q = dbNull $q
+  if ($q) {
+    # Found
+    $folderCache[$name] = $q
+    return $q
+  }
+  # Create new
+  $q = Invoke-SqlScalar @"
+    insert into
+          personal(NAME, TYPE, DESCRIPTION)
+          values (@name, @type, @desq);
+    -- returning id
+    select
+      LAST_INSERT_ID()
+"@ -Parameters @{
+    name = $name
+    type = 'DEP'
+    desq = "Автоматически созданное подразделение для пользователей,`nавторизующихся в домене $name"
+  }
+  $q = dbNull $q
+  $folderCache[$name] = $q
+  return $q
 }
 
 function listOperators {
@@ -52,7 +99,7 @@ function listOperators {
     else {
       $domain = guessDomain $z.id
     }
-    $domain
+    $folder = findFolder $domain
   }
 }
 
