0d81777828cb31311c1feb945f396e8013e7d9a3
Stas Ukolov <ukoloff@gmail.com>
2025-04-08 14:32:35 +0500

    List repos

--- 2trash/gitlab/2gitlab.ps1
+++ 2trash/gitlab/2gitlab.ps1
@@ -1,3 +1,8 @@
 #
 # Mirror repos to GitLab
 #
+Set-Location (Split-Path $PSCommandPath -Parent)
+$root = git rev-parse --show-toplevel
+$repos = Split-Path $root -Parent
+$repos = Get-ChildItem -Path $repos -Directory
+$repos
