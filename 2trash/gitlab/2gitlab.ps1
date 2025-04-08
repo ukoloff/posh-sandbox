e5eb3877092f0e4a15733b1de99d257e0bbc99c6
Stas Ukolov <ukoloff@gmail.com>
2025-04-08 16:26:29 +0500

    Delay for 5 min

--- 2trash/gitlab/2gitlab.ps1
+++ 2trash/gitlab/2gitlab.ps1
@@ -15,6 +15,7 @@ if ($install) {
   $StateChangeTrigger = Get-CimClass -Namespace "root\Microsoft\Windows\TaskScheduler" -ClassName "MSFT_TaskSessionStateChangeTrigger"
   $Trigger = New-CimInstance -CimClass $StateChangeTrigger -Property @{ StateChange = 8 } -ClientOnly
   $Trigger.StateChange = 7 # TASK_SESSION_LOCK https://learn.microsoft.com/en-us/windows/win32/taskschd/sessionstatechangetrigger-statechange
+  $Trigger.Delay = "PT5M"
 
   $Task = New-ScheduledTask -Action $Action -Trigger $Trigger
   Register-ScheduledTask -TaskName $me -TaskPath uxm -InputObject $Task -Force
