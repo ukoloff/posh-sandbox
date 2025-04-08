135f64e9a034b1287c6cff176bf7082e53ce6e7a
Stas Ukolov <ukoloff@gmail.com>
2025-04-08 09:26:25 +0500

    Allow broadcast

--- tsg-log/WoL/readme.md
+++ tsg-log/WoL/readme.md
@@ -1,2 +1,9 @@
 # Wake-on-LAN
 
+
+## Broadcast
+
+Inter-subnet broadcasts (10.164.201.255) seems to be
+rejected by router.
+Can be allowed,
+but not recommended.
--- tsg-log/WoL/send-wol.ps1
+++ tsg-log/WoL/send-wol.ps1
@@ -9,7 +9,14 @@ $ip = '10.164.201.255'
 $macs = $MAC -split "[:-]" | ForEach-Object { [Byte] "0x$_" }
 [Byte[]] $MagicPacket = (, 0xFF * 6) + ($macs * 16)
 
-$udp = New-Object   
+$udp = New-Object System.Net.Sockets.UdpClient
+# https://learn.microsoft.com/en-us/dotnet/api/system.net.sockets.udpclient.client?view=net-9.0
+$uSocket = $udp.Client
+#  use the underlying socket to enable broadcast.
+$uSocket.SetSocketOption([System.Net.Sockets.SocketOptionLevel]::Socket.value__,
+  [System.Net.Sockets.SocketOptionName]::Broadcast.value__,
+  $true)
+
 $udp.Connect($ip, 7)
 $null = $udp.Send($MagicPacket, $MagicPacket.Length)
 $udp.Close()
