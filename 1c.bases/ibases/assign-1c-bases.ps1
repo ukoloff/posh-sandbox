c677876389bf0d953d1748371d0f9b95c0d84d84
Stas Ukolov <ukoloff@gmail.com>
2025-04-09 08:24:54 +0500

    List groups

---
+++ 1c.bases/ibases/assign-1c-bases.ps1
@@ -0,0 +1,8 @@
+﻿#
+# Назначение баз 1С пользователю
+#
+$Groups = 'OU=1CBase,OU=Service,OU=EKBH,OU=uxm,OU=MS,DC=omzglobal,DC=com'
+$stopGroup = 'UZHM-1C не переписывать базы'
+$null = $stopGroup  # ^ Не используется
+
+$Groups = Get-ADGroup -SearchBase $Groups -Filter * -Properties info
