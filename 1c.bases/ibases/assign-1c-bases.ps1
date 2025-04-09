6f9b93608858ac2f8f376324547a9a7dce055b0c
Stas Ukolov <ukoloff@gmail.com>
2025-04-09 10:52:48 +0500

    Start building groups list

--- 1c.bases/ibases/assign-1c-bases.ps1
+++ 1c.bases/ibases/assign-1c-bases.ps1
@@ -7,17 +7,30 @@ $null = $stopGroup  # ^ Не используется
 
 $ini = "$($env:APPDATA)\1C\1CEStart\ibases.v8i"
 
-# List 1C groups
-$s = [ADSISearcher]"(objectClass=group)"
-$s.SearchRoot = [ADSI]"LDAP://$Groups"
-$null = $s.PropertiesToLoad.Add('info')
-$null = $s.PropertiesToLoad.Add('sAMAccountName')
-$g = $s.FindAll()
+# Получаем список групп 1С
+function groups1C {
+  $s = [ADSISearcher]"(objectClass=group)"
+  $s.SearchRoot = [ADSI]"LDAP://$Groups"
+  $null = $s.PropertiesToLoad.Add('info')
+  $null = $s.PropertiesToLoad.Add('sAMAccountName')
 
-# List my groups
-# https://www.secuinfra.com/en/techtalk/adsisearcher-resolve-groups-recursively/
-$me = [ADSISearcher]"(&(objectClass=user)(sAMAccountName=$($env:USERNAME)))"
-$me = $me.FindOne().Properties.distinguishedname
-$s = [ADSISearcher]"(&(objectClass=group)(member:1.2.840.113556.1.4.1941:=$me))"
-$null = $s.PropertiesToLoad.Add('sAMAccountName')
-$myG = $s.FindAll()
+  $result = @{}
+  foreach ($g in $s.FindAll()) {
+    $g = $g.Properties
+    $info = $g.info
+    if ($info) { continue }
+  }
+  $result
+}
+
+# Получаем список всех групп текущего пользователя
+function myGroups {
+  # https://www.secuinfra.com/en/techtalk/adsisearcher-resolve-groups-recursively/
+  $me = [ADSISearcher]"(&(objectClass=user)(sAMAccountName=$($env:USERNAME)))"
+  $me = $me.FindOne().Properties.distinguishedname
+  $s = [ADSISearcher]"(&(objectClass=group)(member:1.2.840.113556.1.4.1941:=$me))"
+  $null = $s.PropertiesToLoad.Add('sAMAccountName')
+  $s.FindAll()
+}
+
+groups1C
