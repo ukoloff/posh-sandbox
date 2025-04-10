debe6fcb3c80a7d4ffc66289fc035b2564689ea1
Stas Ukolov <ukoloff@gmail.com>
2025-04-10 11:58:41 +0500

    Save ARP to Postgres

--- tsg-log/WoL/test/read-arp.ps1
+++ tsg-log/WoL/test/read-arp.ps1
@@ -1,5 +1,30 @@
 #
 # Get & Update ARP table
 #
-Invoke-WebRequest https://nc.ekb.ru/omz/service/arp |
+$cred = Get-StoredCredential -Target pqsql:TSG  # Fallback to Kerberos if not found
+Open-PostGreConnection -Server pg.ekb.ru -Database uxm -Credential $cred
+Start-SqlTransaction
+
+$j = Invoke-WebRequest https://nc.ekb.ru/omz/service/arp |
+Select-Object -ExpandProperty Content |
 ConvertFrom-Json
+$j | ForEach-Object {
+  # https://stackoverflow.com/a/17267423/6127481
+  $null = Invoke-SqlUpdate @"
+    merge into arp using (values
+      (@ip, @mac, clock_timestamp()))
+      as vals (ip, mac, mtime)
+      on arp.ip = vals.ip
+  when matched then
+  update
+    set
+      mac = vals.mac,
+      mtime = vals.mtime
+  when not matched then
+    insert (ip, mac, mtime)
+    values (vals.ip, vals.mac, vals.mtime)
+"@ -ParamObject $_
+}
+
+Complete-SqlTransaction
+Close-SqlConnection
